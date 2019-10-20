87708b77efba958a837a38def909a87768e04e3b
Aron Heck <aron.heck@gmx.de>
2019-10-20 12:55:42 +0200

    fixed comment

--- squidDrone/src/main.cpp
+++ squidDrone/src/main.cpp
@@ -3,8 +3,6 @@
  *
  *  Created on: Oct 4, 2019
  *      Author: aron
- *     TODO static void MX_GPIO_Init(void);
- * 	   TODO static void MX_USART2_UART_Init(void);
  */
 #include "stm32g4xx_hal.h"
 #include "clock_config.h"
