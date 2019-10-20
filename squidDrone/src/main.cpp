f060f34e6bceb27dfc6a44e80bb5661d4040619d
Aron Heck <aron.heck@gmx.de>
2019-10-20 12:44:24 +0200

    Cmake is building and linking startup and config layers. Blinking LED was built and executed. Works. This is proof of concept for CMAKE done. Next up is including the actual project configuration based on what sensors are needed

--- squidDrone/src/main.cpp
+++ squidDrone/src/main.cpp
@@ -8,11 +8,20 @@
  */
 #include "stm32g4xx_hal.h"
 #include "clock_config.h"
+#include "gpio_config.h"
+#include "serial_config.h"
+#include "mcu_settings.h"
 
 int main(){
 
 	HAL_Init();
 	SystemClock_Config();
+	MX_GPIO_Init();
+	MX_USART2_UART_Init();
+	while(1){
+		HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
+		HAL_Delay(500);
+	}
 	return 0;
 }
 
---
+++ squidDrone/src/mcu_config/gpio_config.h
@@ -0,0 +1,21 @@
+/*
+ * gpio_config.h
+ *
+ *  Created on: Oct 20, 2019
+ *      Author: aron
+ */
+
+#ifndef SRC_MCU_CONFIG_GPIO_CONFIG_H_
+#define SRC_MCU_CONFIG_GPIO_CONFIG_H_
+
+#ifdef __cplusplus
+	extern "C" {
+#endif
+
+void MX_GPIO_Init(void);
+
+#ifdef __cplusplus
+	}
+#endif
+
+#endif /* SRC_MCU_CONFIG_GPIO_CONFIG_H_ */
