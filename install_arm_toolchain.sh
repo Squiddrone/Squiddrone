b90fd79a7997581a9f522f2ade4df26fee241963
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2019-11-24 14:10:56 +0100

    Added Travis CI
    Add of build.sh.
    Add of install_arm_toolchain.sh
    Add of .travis.yml.
    Add of Travis CI Badge to README.md.
    Replacement of ${CMAKE_OBJCOPY}  with explicit arm-none-eabi-objcopy in CMakeLists.txt.

---
+++ install_arm_toolchain.sh
@@ -0,0 +1,11 @@
+if [ ! -d $HOME/arm-gcc-toolchain/bin ]; then
+	pushd .
+	cd ~ 
+	if [ ! -d $HOME/arm-gcc-toolchain ]; then
+		mkdir arm-gcc-toolchain
+	fi
+	wget -O $HOME/arm-gcc-toolchain/gcc.tar.bz2 $GCC_URL
+	cd arm-gcc-toolchain
+	tar -jxf gcc.tar.bz2 --strip=1
+	popd
+fi
