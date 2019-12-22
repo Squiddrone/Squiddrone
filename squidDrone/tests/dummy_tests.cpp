46c9d82a6e44ec11193ca8e88319fb3101361402
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2019-12-22 11:50:01 +0100

    Add of GTest and Coverage
    Not buildable yet with GTest ON.
    Linking Problems.

---
+++ squidDrone/tests/dummy_tests.cpp
@@ -0,0 +1,17 @@
+
+#include "gtest/gtest.h"
+
+namespace {
+  TEST(dummy, trivial_pass) {
+    EXPECT_EQ(true, true);
+  }
+
+  TEST(dummy, trivial_faile) {
+    EXPECT_EQ(true, false);
+  }
+}
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
