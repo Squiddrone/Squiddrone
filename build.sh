3d28e2e615a90e16e8c7de1d514e9df08046e1a2
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2019-12-26 10:03:33 +0100

    Removal of Code Style Violations
    Reported by Codacy.

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ mkdir -p build && cd build
 # Configure
 cmake ../ -DCMAKE_TOOLCHAIN_FILE=../cmake/gnu_arm_stm32_toolchain.cmake
 # Build (for Make on Unix equivalent to `make -j $(nproc)`)
-cmake --build . -j $(nproc)
+cmake --build . -j "$(nproc)"
--- install_arm_toolchain.sh
+++ install_arm_toolchain.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ ! -d "$HOME/arm-gcc-toolchain/bin" ]; then
-	pushd .
-	cd ~ 
-	if [ ! -d "$HOME/arm-gcc-toolchain" ]; then
-		mkdir arm-gcc-toolchain
-	fi
-	wget -O "$HOME/arm-gcc-toolchain/gcc.tar.bz2" "$GCC_URL"
-	cd arm-gcc-toolchain
-	tar -jxf gcc.tar.bz2 --strip=1
-	rm -f gcc.tar.bz2
-	popd
+  pushd .
+  cd ~ || exit
+  if [ ! -d "$HOME/arm-gcc-toolchain" ]; then
+    mkdir arm-gcc-toolchain
+  fi
+  wget -O "$HOME/arm-gcc-toolchain/gcc.tar.bz2" "$GCC_URL"
+  cd arm-gcc-toolchain || exit
+  tar -jxf gcc.tar.bz2 --strip=1
+  rm -f gcc.tar.bz2
+  popd || exit
 fi
