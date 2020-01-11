ac99317de1bad32c5c062a9a935d4cdf1d02efb5
Aron <aron.heck@gmx.de>
2020-01-11 22:59:54 +0100

    Wrote doxygen documentation

--- squidDrone/src/propulsion/esc.hpp
+++ squidDrone/src/propulsion/esc.hpp
@@ -3,16 +3,43 @@
 
 #include "stm32g4xx_hal_tim.h"
 
-class Esc{
-  public:
-    explicit Esc() = default;
-    explicit Esc(TIM_HandleTypeDef timer): timer_(timer){};
+namespace propulsion{
 
-  protected:
-    TIM_HandleTypeDef timer_;
-};  
+  ///
+  /// @class Esc 
+  /// @brief The abstract Esc provides a interface for concrete Esc's to implement
+  /// The Esc abstract interface dictates the methods which all concrete Esc's have to
+  /// implement. 
+  ///
+  class Esc{
+    public:
+      ///
+      /// @brief  Default constructor may not be used, so
+      ///         it is deleted.
+      ///
+      Esc() = delete;
 
+      ///
+      /// @brief  The single argument custom constructor is
+      ///         the one to be used. 
+      /// @param  timer The timer handle struct defined by hal driver 
+      ///               stm32g4xx_hal_tim.h. 
+      /// The only thing this constructor does, is to set the protected 
+      /// member variable timer_.
+      ///
+      explicit Esc(TIM_HandleTypeDef timer): timer_(timer){};
 
+    protected:
+      
+      ///
+      /// Hal provided timer interface is protected, so
+      /// the concrete implementation can use it.
+      ///
+      TIM_HandleTypeDef timer_;
+      
+  };  
+
+}
 
 
 
