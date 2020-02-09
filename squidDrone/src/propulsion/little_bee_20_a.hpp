b16d2f0bf60986bb1c1e28ad64fb1864ba952ab7
Aron <aron.heck@gmx.de>
2020-02-09 14:10:50 +0100

    Added commentary and fixed codacy issues

--- squidDrone/src/propulsion/little_bee_20_a.hpp
+++ squidDrone/src/propulsion/little_bee_20_a.hpp
@@ -6,20 +6,25 @@
 
 
 namespace propulsion{
-    
+  ///
+  /// @class LittleBee20A 
+  /// @brief The concrete class implementation of the abstract Esc class for
+  /// the Little Bee 20 a hardware.
+  ///
   class LittleBee20A final: public Esc{
     
     public:
-      
+      /// @brief Default constructor is deleted, because implementation need timer
       LittleBee20A() = delete;
 
+      /// @brief Custom constructor with needed timer
       explicit LittleBee20A(TIM_HandleTypeDef& timer);
 
-      const auto GetMaxPulseDurationInMicroSeconds() const noexcept -> decltype(auto);
+      const auto GetMaxPulseDurationInMicroSeconds() const noexcept -> decltype(auto) override;
 
-      const auto GetMinPulseDurationInMicroSeconds() const noexcept -> decltype(auto);
+      const auto GetMinPulseDurationInMicroSeconds() const noexcept -> decltype(auto) override;
 
-      const auto SetPulseDuration(int pulse_duration) noexcept -> decltype(types::HalError::working);
+      const auto SetPulseDuration(int pulse_duration) noexcept -> decltype(types::HalError::working) override;
 
   };
 
