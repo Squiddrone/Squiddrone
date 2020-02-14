6272b69685b0756d7a3057113de232f231a27fec
Aron <aron.heck@gmx.de>
2020-02-14 20:54:20 +0100

    Broke stm32g4x_ha_tim into  a c and h file, otherwise struct instances  would be multiply defined

--- squidDrone/tests/mock_libraries/stm32g4xx_hal_tim.h
+++ squidDrone/tests/mock_libraries/stm32g4xx_hal_tim.h
@@ -44,13 +44,9 @@ typedef struct {
   HAL_StatusTypeDef return_value;
 } HalTimPwmStopMockValues;
 
-HalTimPwmStopMockValues hal_tim_pwm_stop_mock_values;
+extern HalTimPwmStopMockValues hal_tim_pwm_stop_mock_values;
 
-HAL_StatusTypeDef HAL_TIM_PWM_Stop(TIM_HandleTypeDef *htim, uint32_t Channel){
-  hal_tim_pwm_stop_mock_values.htim = htim;
-  hal_tim_pwm_stop_mock_values.channel = Channel;
-  return hal_tim_pwm_stop_mock_values.return_value;
-}
+HAL_StatusTypeDef HAL_TIM_PWM_Stop(TIM_HandleTypeDef *htim, uint32_t Channel);
 
 typedef struct {
   int timer_clock;
@@ -58,13 +54,9 @@ typedef struct {
   uint32_t return_value;
 }HalTimCalcPscMockValues;
 
-HalTimCalcPscMockValues hal_tim_calc_psc_mock_values;
+extern HalTimCalcPscMockValues hal_tim_calc_psc_mock_values;
 
-uint32_t __HAL_TIM_CALC_PSC(int timer_clock, int target_timer_clock_rate){
-  hal_tim_calc_psc_mock_values.timer_clock = timer_clock;
-  hal_tim_calc_psc_mock_values.target_timer_clock_rate = target_timer_clock_rate;
-  return hal_tim_calc_psc_mock_values.return_value;
-}
+uint32_t __HAL_TIM_CALC_PSC(int timer_clock, int target_timer_clock_rate);
 
 typedef struct
 {
@@ -81,26 +73,18 @@ typedef struct{
   HAL_StatusTypeDef return_value;
 }HalTimPwmConfigChannelMockValues;
 
-HalTimPwmConfigChannelMockValues hal_tim_pwm_config_channel_mock_values;
+extern HalTimPwmConfigChannelMockValues hal_tim_pwm_config_channel_mock_values;
 
-HAL_StatusTypeDef HAL_TIM_PWM_ConfigChannel(TIM_HandleTypeDef *htim, TIM_OC_InitTypeDef *sConfig, uint32_t Channel){
-  hal_tim_pwm_config_channel_mock_values.htim = htim;
-  hal_tim_pwm_config_channel_mock_values.sconfig = sConfig;
-  hal_tim_pwm_config_channel_mock_values.channel = Channel;
-  return hal_tim_pwm_config_channel_mock_values.return_value;
-}
+HAL_StatusTypeDef HAL_TIM_PWM_ConfigChannel(TIM_HandleTypeDef *htim, TIM_OC_InitTypeDef *sConfig, uint32_t Channel);
 
 typedef struct{
   TIM_HandleTypeDef* htim;
   HAL_StatusTypeDef return_value;
 } HalTimPwmInitMockValues;
 
-HalTimPwmInitMockValues hal_tim_pwm_init_mock_values;
+extern HalTimPwmInitMockValues hal_tim_pwm_init_mock_values;
 
-HAL_StatusTypeDef HAL_TIM_PWM_Init(TIM_HandleTypeDef *htim){
-  hal_tim_pwm_init_mock_values.htim = htim;
-  return hal_tim_pwm_init_mock_values.return_value;
-}
+HAL_StatusTypeDef HAL_TIM_PWM_Init(TIM_HandleTypeDef *htim);
 
 typedef struct{
   TIM_HandleTypeDef *htim;
@@ -108,13 +92,9 @@ typedef struct{
   HAL_StatusTypeDef return_value;
 }HalTimPwmStartMockValues;
 
-HalTimPwmStartMockValues hal_tim_pwm_start_mock_values;
+extern HalTimPwmStartMockValues hal_tim_pwm_start_mock_values;
 
-HAL_StatusTypeDef HAL_TIM_PWM_Start(TIM_HandleTypeDef *htim, uint32_t Channel){
-  hal_tim_pwm_start_mock_values.htim = htim;
-  hal_tim_pwm_start_mock_values.channel = Channel;
-  return hal_tim_pwm_start_mock_values.return_value;
-}
+HAL_StatusTypeDef HAL_TIM_PWM_Start(TIM_HandleTypeDef *htim, uint32_t Channel);
 
 #ifdef __cplusplus
 }
--- squidDrone/tests/propulsion/little_bee_20_a_test.cpp
+++ squidDrone/tests/propulsion/little_bee_20_a_test.cpp
@@ -1,4 +1,5 @@
 #include <memory>
+#include <utility>
 #include "gtest/gtest.h"
 #include "little_bee_20_a.hpp"
 #include "stm32g4xx.h"
@@ -8,16 +9,24 @@ namespace{
   class LittleBeeEscTests: public ::testing::Test{
     protected:
       virtual void SetUp(){
-        
-        unit_under_test_ = new propulsion::LittleBee20A(&mock_timer);
+        unit_under_test_ = std::make_unique<propulsion::LittleBee20A>(&mock_timer);
       }
+
       TIM_HandleTypeDef mock_timer;
-      propulsion::LittleBee20A* unit_under_test_;
+      std::unique_ptr<propulsion::LittleBee20A> unit_under_test_;
   };
 
   TEST_F(LittleBeeEscTests, get_max_pulse_duration_in_micro_seconds_return_correct_value){
     ASSERT_EQ(unit_under_test_->GetMaxPulseDurationInMicroSeconds(), 250);
   }
+
+  TEST_F(LittleBeeEscTests, get_min_pulse_duration_in_micro_seconds_return_correct_value){
+    ASSERT_EQ(unit_under_test_->GetMinPulseDurationInMicroSeconds(), 125);
+  }
+
+  
+
+
 }
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
