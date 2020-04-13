4a1383c378fea5e6db040df5510afb1368c6c513
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-04-13 16:26:41 +0200

    Add of unit tests for accelerometer get and set of sensitivity

--- squidDrone/tests/imu/imu_tests.cpp
+++ squidDrone/tests/imu/imu_tests.cpp
@@ -47,6 +47,49 @@ namespace{
     EXPECT_EQ(gyroscope_sensitivity_return, sensitivity);
   }
 
+  TEST(imu, imu_interface_set_accelerometer_sensitivity_finest){
+    i2c::I2CHandler i2c_handler;
+    imu::InertialMeasurementInterface imu_interface(i2c_handler);
+    types::AccelerometerSensitivity sensitivity = types::AccelerometerSensitivity::FINEST;
+    imu_interface.SetAccelerometerSensitivity(sensitivity);
+    types::AccelerometerSensitivity gyroscope_sensitivity_return = imu_interface.GetAccelerometerSensitivity();
+    EXPECT_EQ(gyroscope_sensitivity_return, sensitivity);
+  }
+
+  TEST(imu, imu_interface_set_accelerometer_sensitivity_finer){
+    i2c::I2CHandler i2c_handler;
+    imu::InertialMeasurementInterface imu_interface(i2c_handler);
+    types::AccelerometerSensitivity sensitivity = types::AccelerometerSensitivity::FINER;
+    imu_interface.SetAccelerometerSensitivity(sensitivity);
+    types::AccelerometerSensitivity gyroscope_sensitivity_return = imu_interface.GetAccelerometerSensitivity();
+    EXPECT_EQ(gyroscope_sensitivity_return, sensitivity);
+  }
+
+  TEST(imu, imu_interface_set_accelerometer_sensitivity_rougher){
+    i2c::I2CHandler i2c_handler;
+    imu::InertialMeasurementInterface imu_interface(i2c_handler);
+    types::AccelerometerSensitivity sensitivity = types::AccelerometerSensitivity::ROUGHER;
+    imu_interface.SetAccelerometerSensitivity(sensitivity);
+    types::AccelerometerSensitivity gyroscope_sensitivity_return = imu_interface.GetAccelerometerSensitivity();
+    EXPECT_EQ(gyroscope_sensitivity_return, sensitivity);
+  }
+
+  TEST(imu, imu_interface_set_accelerometer_sensitivity_roughest){
+    i2c::I2CHandler i2c_handler;
+    imu::InertialMeasurementInterface imu_interface(i2c_handler);
+    types::AccelerometerSensitivity sensitivity = types::AccelerometerSensitivity::ROUGHEST;
+    imu_interface.SetAccelerometerSensitivity(sensitivity);
+    types::AccelerometerSensitivity gyroscope_sensitivity_return = imu_interface.GetAccelerometerSensitivity();
+    EXPECT_EQ(gyroscope_sensitivity_return, sensitivity);
+  }
+
+  TEST(imu, imu_interface_get_accelerometer_sensitivity_default){
+    i2c::I2CHandler i2c_handler;
+    imu::InertialMeasurementInterface imu_interface(i2c_handler);
+    types::AccelerometerSensitivity sensitivity = types::AccelerometerSensitivity::FINEST;
+    types::AccelerometerSensitivity gyroscope_sensitivity_return = imu_interface.GetAccelerometerSensitivity();
+    EXPECT_EQ(gyroscope_sensitivity_return, sensitivity);
+  }
 }
 
 int main(int argc, char **argv) {
