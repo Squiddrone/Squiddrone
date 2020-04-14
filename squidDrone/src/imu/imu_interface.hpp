999e496ac8d3a56b0bf81826208a0bac67f9eedf
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-04-14 17:16:03 +0200

    Fix of linking error
    All methods have to be pure virtual (=0). Thanks to @elheck.

--- squidDrone/src/imu/imu_interface.hpp
+++ squidDrone/src/imu/imu_interface.hpp
@@ -16,14 +16,14 @@ namespace imu
       InertialMeasurementInterface() = delete;
       virtual ~InertialMeasurementInterface() = default;
       explicit InertialMeasurementInterface(i2c::I2CHandler i2c_handler): i2c_handler_(i2c_handler){};
-      virtual void SetGyroscopeSensitivity(types::GyroscopeSensitivity gyroscope_sensitivity) noexcept;
-      virtual types::GyroscopeSensitivity GetGyroscopeSensitivity(void) noexcept;
-      virtual void SetAccelerometerSensitivity(types::AccelerometerSensitivity accelerometer_sensitivity) noexcept;
-      virtual types::AccelerometerSensitivity GetAccelerometerSensitivity(void) noexcept;
-      virtual types::EuclideanVector<float> GetGyroscope(void) noexcept;
-      virtual types::EuclideanVector<float> GetAccelerometer(void) noexcept;
-      virtual types::EuclideanVector<float> GetMagnetometer(void) noexcept;
-      virtual int GetTemperature(void) noexcept;
+      virtual void SetGyroscopeSensitivity(types::GyroscopeSensitivity gyroscope_sensitivity) noexcept = 0;
+      virtual types::GyroscopeSensitivity GetGyroscopeSensitivity(void) noexcept = 0;
+      virtual void SetAccelerometerSensitivity(types::AccelerometerSensitivity accelerometer_sensitivity) noexcept = 0;
+      virtual types::AccelerometerSensitivity GetAccelerometerSensitivity(void) noexcept = 0;
+      virtual types::EuclideanVector<float> GetGyroscope(void) noexcept = 0;
+      virtual types::EuclideanVector<float> GetAccelerometer(void) noexcept = 0;
+      virtual types::EuclideanVector<float> GetMagnetometer(void) noexcept = 0;
+      virtual int GetTemperature(void) noexcept = 0;
     protected:
       i2c::I2CHandler i2c_handler_;
   };
