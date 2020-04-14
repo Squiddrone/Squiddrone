a4d080c0fbb68ee3b0786e82c4bfa59e8fdaf427
Aron <aron.heck@gmx.de>
2020-04-14 18:26:10 +0200

    implemented letodar - and started to do the rest of the tests

--- squidDrone/src/propulsion/letodar_2204.hpp
+++ squidDrone/src/propulsion/letodar_2204.hpp
@@ -8,10 +8,17 @@ namespace propulsion{
   class LeTodar2204 final: public Motor{
     public:
       LeTodar2204() = delete;
-      explicit LeTodar2204(std::unique_ptr<Esc> esc): Motor(std::move(esc)){}
+      explicit LeTodar2204(std::unique_ptr<Esc> esc): Motor(std::move(esc)), speed_(0.0){}
 
       auto GetCurrentSpeedInPercent() const noexcept -> const float override;
       auto SetSpeedInPercent(const float speed) noexcept -> types::InputError override;
+
+    private:
+      static constexpr int REPETITION_TIME_IN_MS = 500;
+      static constexpr auto UPPER_LEGAL_RANGE_LIMIT_IN_PERCENT = 100.0;
+      static constexpr auto LOWER_LEGAL_RANGE_LIMIT_IN_PERCENT = 0.0;
+      static constexpr auto PERCENTAGE_FACTOR = 100.0;
+      float speed_;
   };
 }
 
--- squidDrone/tests/mock_libraries/propulsion_mock_dependencies.hpp
+++ squidDrone/tests/mock_libraries/propulsion_mock_dependencies.hpp
@@ -1,3 +1,14 @@
 namespace propulsion{
-  class Esc{};
+  class Esc{
+    public:
+      auto GetMaxPulseDurationInMicroSeconds() const noexcept -> decltype(auto){
+        return 100;
+      }
+
+      auto GetMinPulseDurationInMicroSeconds() const noexcept -> decltype(auto){
+        return 10;
+      }
+
+      auto SetPulseDuration(int pulse_duration, int repetition_period) noexcept -> types::HalError;
+  };
 }
