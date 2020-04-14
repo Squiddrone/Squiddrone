0bf7b88702c7ff03ac13132792725982f382908c
Aron <aron.heck@gmx.de>
2020-04-14 17:51:39 +0200

    Unittests done

--- squidDrone/src/propulsion/letodar_2204.cpp
+++ squidDrone/src/propulsion/letodar_2204.cpp
@@ -1,1 +1,12 @@
 #include "letodar_2204.hpp"
+
+namespace propulsion{
+
+  auto LeTodar2204::GetCurrentSpeedInPercent() const noexcept -> const float{
+
+  }
+  
+  auto LeTodar2204::SetSpeedInPercent(const float speed) noexcept -> types::InputError{
+
+  }
+}
--- squidDrone/tests/propulsion/letodar_2204_test.cpp
+++ squidDrone/tests/propulsion/letodar_2204_test.cpp
@@ -1,4 +1,41 @@
+#include <utility>
 #include "gtest/gtest.h"
+#include "letodar_2204.hpp"
+
+namespace{
+  class Letodar2204Tests: public ::testing::Test{
+    protected:
+      virtual void SetUp(){
+        unit_under_test_ = std::make_unique<propulsion::LeTodar2204>(std::make_unique<propulsion::Esc>(esc_));
+      }
+
+      propulsion::Esc esc_;
+      std::unique_ptr<propulsion::LeTodar2204> unit_under_test_;
+  };
+
+  TEST_F(Letodar2204Tests, get_current_speed_in_percent_works){
+    auto current_speed = unit_under_test_->GetCurrentSpeedInPercent();
+    ASSERT_FLOAT_EQ(0.0, current_speed);
+  }
+
+  TEST_F(Letodar2204Tests, set_speed_in_percent_lower_illegal_bounds){
+    auto set_speed_return = unit_under_test_->SetSpeedInPercent(-1.0);
+    ASSERT_EQ(types::InputError::INPUT_OUT_OF_RANGE, set_speed_return);
+  }
+
+  TEST_F(Letodar2204Tests, set_speed_in_percent_upper_illegal_bounds){
+    auto set_speed_return = unit_under_test_->SetSpeedInPercent(100.1);
+    ASSERT_EQ(types::InputError::INPUT_OUT_OF_RANGE, set_speed_return);
+  }
+
+  TEST_F(Letodar2204Tests, set_speed_in_percent_legal_range){
+    auto set_speed_return = unit_under_test_->SetSpeedInPercent(50.0);
+    ASSERT_EQ(types::InputError::INPUT_CORRECT, set_speed_return);
+  }
+}
+
+
+
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
