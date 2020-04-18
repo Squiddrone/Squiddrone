de57516de9ecbb2817d3820b6831660b9a718810
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-04-18 11:15:54 +0200

    Adapted StyleGuide

--- squidDrone/src/imu/inertial_measurement.cpp
+++ squidDrone/src/imu/inertial_measurement.cpp
@@ -3,44 +3,36 @@
 namespace imu
 {
 
-void InertialMeasurement::SetGyroscopeSensitivity(types::GyroscopeSensitivity gyroscope_sensitivity) noexcept
-{
-  mpu9250.SetGyroscopeSensitivity(gyroscope_sensitivity);
-}
+  void InertialMeasurement::SetGyroscopeSensitivity(types::GyroscopeSensitivity gyroscope_sensitivity) noexcept {
+    mpu9250.SetGyroscopeSensitivity(gyroscope_sensitivity);
+  }
 
-types::GyroscopeSensitivity InertialMeasurement::GetGyroscopeSensitivity(void) noexcept
-{
-  return mpu9250.GetGyroscopeSensitivity();
-}
+  types::GyroscopeSensitivity InertialMeasurement::GetGyroscopeSensitivity(void) noexcept {
+    return mpu9250.GetGyroscopeSensitivity();
+  }
 
-void InertialMeasurement::SetAccelerometerSensitivity(types::AccelerometerSensitivity accelerometer_sensitivity) noexcept
-{
-  mpu9250.SetAccelerometerSensitivity(accelerometer_sensitivity);
-}
+  void InertialMeasurement::SetAccelerometerSensitivity(types::AccelerometerSensitivity accelerometer_sensitivity) noexcept {
+    mpu9250.SetAccelerometerSensitivity(accelerometer_sensitivity);
+  }
 
-types::AccelerometerSensitivity InertialMeasurement::GetAccelerometerSensitivity(void) noexcept
-{
-  return mpu9250.GetAccelerometerSensitivity();
-}
+  types::AccelerometerSensitivity InertialMeasurement::GetAccelerometerSensitivity(void) noexcept {
+    return mpu9250.GetAccelerometerSensitivity();
+  }
 
-types::EuclideanVector<float> InertialMeasurement::GetGyroscope(void) noexcept
-{
-  return mpu9250.GetGyroscope();
-}
+  types::EuclideanVector<float> InertialMeasurement::GetGyroscope(void) noexcept {
+    return mpu9250.GetGyroscope();
+  }
 
-types::EuclideanVector<float> InertialMeasurement::GetAccelerometer(void) noexcept
-{
-  return mpu9250.GetAccelerometer();
-}
+  types::EuclideanVector<float> InertialMeasurement::GetAccelerometer(void) noexcept {
+    return mpu9250.GetAccelerometer();
+  }
 
-types::EuclideanVector<float> InertialMeasurement::GetMagnetometer(void) noexcept
-{
-  return mpu9250.GetMagnetometer();
-}
+  types::EuclideanVector<float> InertialMeasurement::GetMagnetometer(void) noexcept {
+    return mpu9250.GetMagnetometer();
+  }
 
-int InertialMeasurement::GetTemperature(void) noexcept
-{
-  return mpu9250.GetTemperature();
-}
+  int InertialMeasurement::GetTemperature(void) noexcept {
+    return mpu9250.GetTemperature();
+  }
 
 } // namespace imu
