141339631b370067f6b9f2acb07c40426d7b94e1
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-04-18 11:16:35 +0200

    Adapted StyleGuide

--- squidDrone/tests/mock_libraries/imu/mpu9250.cpp
+++ squidDrone/tests/mock_libraries/imu/mpu9250.cpp
@@ -2,39 +2,39 @@
 
 namespace imu {
 
-    void Mpu9250::SetGyroscopeSensitivity(types::GyroscopeSensitivity gyroscope_sensitivity) noexcept {
+  void Mpu9250::SetGyroscopeSensitivity(types::GyroscopeSensitivity gyroscope_sensitivity) noexcept {
 
-    }
+  }
 
-    types::GyroscopeSensitivity Mpu9250::GetGyroscopeSensitivity(void) noexcept {
-      return gyroscope_sensitivity;
-    }
+  types::GyroscopeSensitivity Mpu9250::GetGyroscopeSensitivity(void) noexcept {
+    return gyroscope_sensitivity;
+  }
 
-    void Mpu9250::SetAccelerometerSensitivity(types::AccelerometerSensitivity accelerometer_sensitivity) noexcept {
+  void Mpu9250::SetAccelerometerSensitivity(types::AccelerometerSensitivity accelerometer_sensitivity) noexcept {
 
-    }
+  }
 
-    types::AccelerometerSensitivity Mpu9250::GetAccelerometerSensitivity(void) noexcept {
-      return accelerometer_sensitivity;
-    }
+  types::AccelerometerSensitivity Mpu9250::GetAccelerometerSensitivity(void) noexcept {
+    return accelerometer_sensitivity;
+  }
 
-    types::EuclideanVector<float> Mpu9250::GetGyroscope(void) noexcept {
-      types::EuclideanVector<float> value{1.5, 2.5, 3.5};
-      return value;
-    }
+  types::EuclideanVector<float> Mpu9250::GetGyroscope(void) noexcept {
+    types::EuclideanVector<float> value{1.5, 2.5, 3.5};
+    return value;
+  }
 
-    types::EuclideanVector<float> Mpu9250::GetAccelerometer(void) noexcept {
-      types::EuclideanVector<float> value{4.5, 5.5, 6.5};
-      return value;
-    }
+  types::EuclideanVector<float> Mpu9250::GetAccelerometer(void) noexcept {
+    types::EuclideanVector<float> value{4.5, 5.5, 6.5};
+    return value;
+  }
 
-    types::EuclideanVector<float> Mpu9250::GetMagnetometer(void) noexcept {
-      types::EuclideanVector<float> value{7.5, 8.5, 9.5};
-      return value;
-    }
+  types::EuclideanVector<float> Mpu9250::GetMagnetometer(void) noexcept {
+    types::EuclideanVector<float> value{7.5, 8.5, 9.5};
+    return value;
+  }
 
-    int Mpu9250::GetTemperature(void) noexcept { 
-      return 17; 
-    }
+  int Mpu9250::GetTemperature(void) noexcept { 
+    return 17; 
+  }
 
 }
