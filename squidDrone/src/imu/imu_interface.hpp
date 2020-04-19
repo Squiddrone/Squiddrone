8ff2bd831228f0366eb6e520783af267096f0c61
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-04-19 12:57:09 +0200

    Removal of unnecessary attribute

--- squidDrone/src/imu/imu_interface.hpp
+++ squidDrone/src/imu/imu_interface.hpp
@@ -15,7 +15,7 @@ namespace imu
     public:
       InertialMeasurementInterface() = delete;
       virtual ~InertialMeasurementInterface() = default;
-      explicit InertialMeasurementInterface(std::unique_ptr<i2c::I2CHandler> i2c_handler): i2c_handler_(std::move(i2c_handler)){};
+      explicit InertialMeasurementInterface(std::unique_ptr<i2c::I2CHandler> i2c_handler){};
       virtual void SetGyroscopeSensitivity(types::GyroscopeSensitivity gyroscope_sensitivity) noexcept = 0;
       virtual types::GyroscopeSensitivity GetGyroscopeSensitivity(void) noexcept = 0;
       virtual void SetAccelerometerSensitivity(types::AccelerometerSensitivity accelerometer_sensitivity) noexcept = 0;
@@ -24,8 +24,6 @@ namespace imu
       virtual types::EuclideanVector<float> GetAccelerometer(void) noexcept = 0;
       virtual types::EuclideanVector<float> GetMagnetometer(void) noexcept = 0;
       virtual int GetTemperature(void) noexcept = 0;
-    protected:
-      std::unique_ptr<i2c::I2CHandler> i2c_handler_;
   };
 
 } // namespace imu
