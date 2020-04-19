5422dbfde15137d6a24c98f12882210fdcc1327b
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-04-19 15:45:24 +0200

    Refactor of unit test to use googletests ::testing::Test with SetUp function

--- squidDrone/tests/imu/imu_interface_tests.cpp
+++ squidDrone/tests/imu/imu_interface_tests.cpp
@@ -4,129 +4,112 @@
 
 namespace{
 
-  TEST(imu, interface_set_gyroscope_sensitivity_finest){
-    std::unique_ptr<i2c::I2CHandler> i2c_handler = std::make_unique<i2c::I2CHandler>();
-    imu::InertialMeasurement imu_interface(std::move(i2c_handler));
+  class ImuInterfaceTests: public ::testing::Test{
+  protected:
+    virtual void SetUp(){
+      std::unique_ptr<i2c::I2CHandler> i2c_handler_ = std::make_unique<i2c::I2CHandler>();
+      unit_under_test_ = std::make_unique<imu::InertialMeasurement>(std::move(i2c_handler_));
+    }
+
+    std::unique_ptr<i2c::I2CHandler> i2c_handler_;
+    std::unique_ptr<imu::InertialMeasurement> unit_under_test_;
+  };
+
+  TEST_F(ImuInterfaceTests, interface_set_gyroscope_sensitivity_finest){
     types::GyroscopeSensitivity sensitivity = types::GyroscopeSensitivity::FINEST;
-    imu_interface.SetGyroscopeSensitivity(sensitivity);
-    types::GyroscopeSensitivity gyroscope_sensitivity_return = imu_interface.GetGyroscopeSensitivity();
+    unit_under_test_->SetGyroscopeSensitivity(sensitivity);
+    types::GyroscopeSensitivity gyroscope_sensitivity_return = unit_under_test_->GetGyroscopeSensitivity();
     EXPECT_EQ(gyroscope_sensitivity_return, sensitivity);
   }
 
-  TEST(imu, interface_set_gyroscope_sensitivity_finer){
-    std::unique_ptr<i2c::I2CHandler> i2c_handler = std::make_unique<i2c::I2CHandler>();
-    imu::InertialMeasurement imu_interface(std::move(i2c_handler));
+  TEST_F(ImuInterfaceTests, interface_set_gyroscope_sensitivity_finer){
     types::GyroscopeSensitivity sensitivity = types::GyroscopeSensitivity::FINER;
-    imu_interface.SetGyroscopeSensitivity(sensitivity);
-    types::GyroscopeSensitivity gyroscope_sensitivity_return = imu_interface.GetGyroscopeSensitivity();
+    unit_under_test_->SetGyroscopeSensitivity(sensitivity);
+    types::GyroscopeSensitivity gyroscope_sensitivity_return = unit_under_test_->GetGyroscopeSensitivity();
     EXPECT_EQ(gyroscope_sensitivity_return, sensitivity);
   }
 
-  TEST(imu, interface_set_gyroscope_sensitivity_rougher){
-    std::unique_ptr<i2c::I2CHandler> i2c_handler = std::make_unique<i2c::I2CHandler>();
-    imu::InertialMeasurement imu_interface(std::move(i2c_handler));
+  TEST_F(ImuInterfaceTests, interface_set_gyroscope_sensitivity_rougher){
     types::GyroscopeSensitivity sensitivity = types::GyroscopeSensitivity::ROUGHER;
-    imu_interface.SetGyroscopeSensitivity(sensitivity);
-    types::GyroscopeSensitivity gyroscope_sensitivity_return = imu_interface.GetGyroscopeSensitivity();
+    unit_under_test_->SetGyroscopeSensitivity(sensitivity);
+    types::GyroscopeSensitivity gyroscope_sensitivity_return = unit_under_test_->GetGyroscopeSensitivity();
     EXPECT_EQ(gyroscope_sensitivity_return, sensitivity);
   }
 
-  TEST(imu, interface_set_gyroscope_sensitivity_roughest){
-    std::unique_ptr<i2c::I2CHandler> i2c_handler = std::make_unique<i2c::I2CHandler>();
-    imu::InertialMeasurement imu_interface(std::move(i2c_handler));
+  TEST_F(ImuInterfaceTests, interface_set_gyroscope_sensitivity_roughest){
     types::GyroscopeSensitivity sensitivity = types::GyroscopeSensitivity::ROUGHEST;
-    imu_interface.SetGyroscopeSensitivity(sensitivity);
-    types::GyroscopeSensitivity gyroscope_sensitivity_return = imu_interface.GetGyroscopeSensitivity();
+    unit_under_test_->SetGyroscopeSensitivity(sensitivity);
+    types::GyroscopeSensitivity gyroscope_sensitivity_return = unit_under_test_->GetGyroscopeSensitivity();
     EXPECT_EQ(gyroscope_sensitivity_return, sensitivity);
   }
 
-  TEST(imu, interface_get_gyroscope_sensitivity_default){
-    std::unique_ptr<i2c::I2CHandler> i2c_handler = std::make_unique<i2c::I2CHandler>();
-    imu::InertialMeasurement imu_interface(std::move(i2c_handler));
+  TEST_F(ImuInterfaceTests, interface_get_gyroscope_sensitivity_default){
     types::GyroscopeSensitivity sensitivity = types::GyroscopeSensitivity::FINEST;
-    types::GyroscopeSensitivity gyroscope_sensitivity_return = imu_interface.GetGyroscopeSensitivity();
+    types::GyroscopeSensitivity gyroscope_sensitivity_return = unit_under_test_->GetGyroscopeSensitivity();
     EXPECT_EQ(gyroscope_sensitivity_return, sensitivity);
   }
 
-  TEST(imu, interface_set_accelerometer_sensitivity_finest){
-    std::unique_ptr<i2c::I2CHandler> i2c_handler = std::make_unique<i2c::I2CHandler>();
-    imu::InertialMeasurement imu_interface(std::move(i2c_handler));
+  TEST_F(ImuInterfaceTests, interface_set_accelerometer_sensitivity_finest){
     types::AccelerometerSensitivity sensitivity = types::AccelerometerSensitivity::FINEST;
-    imu_interface.SetAccelerometerSensitivity(sensitivity);
-    types::AccelerometerSensitivity gyroscope_sensitivity_return = imu_interface.GetAccelerometerSensitivity();
+    unit_under_test_->SetAccelerometerSensitivity(sensitivity);
+    types::AccelerometerSensitivity gyroscope_sensitivity_return = unit_under_test_->GetAccelerometerSensitivity();
     EXPECT_EQ(gyroscope_sensitivity_return, sensitivity);
   }
 
-  TEST(imu, interface_set_accelerometer_sensitivity_finer){
-    std::unique_ptr<i2c::I2CHandler> i2c_handler = std::make_unique<i2c::I2CHandler>();
-    imu::InertialMeasurement imu_interface(std::move(i2c_handler));
+  TEST_F(ImuInterfaceTests, interface_set_accelerometer_sensitivity_finer){
     types::AccelerometerSensitivity sensitivity = types::AccelerometerSensitivity::FINER;
-    imu_interface.SetAccelerometerSensitivity(sensitivity);
-    types::AccelerometerSensitivity gyroscope_sensitivity_return = imu_interface.GetAccelerometerSensitivity();
+    unit_under_test_->SetAccelerometerSensitivity(sensitivity);
+    types::AccelerometerSensitivity gyroscope_sensitivity_return = unit_under_test_->GetAccelerometerSensitivity();
     EXPECT_EQ(gyroscope_sensitivity_return, sensitivity);
   }
 
-  TEST(imu, interface_set_accelerometer_sensitivity_rougher){
-    std::unique_ptr<i2c::I2CHandler> i2c_handler = std::make_unique<i2c::I2CHandler>();
-    imu::InertialMeasurement imu_interface(std::move(i2c_handler));
+  TEST_F(ImuInterfaceTests, interface_set_accelerometer_sensitivity_rougher){
     types::AccelerometerSensitivity sensitivity = types::AccelerometerSensitivity::ROUGHER;
-    imu_interface.SetAccelerometerSensitivity(sensitivity);
-    types::AccelerometerSensitivity gyroscope_sensitivity_return = imu_interface.GetAccelerometerSensitivity();
+    unit_under_test_->SetAccelerometerSensitivity(sensitivity);
+    types::AccelerometerSensitivity gyroscope_sensitivity_return = unit_under_test_->GetAccelerometerSensitivity();
     EXPECT_EQ(gyroscope_sensitivity_return, sensitivity);
   }
 
-  TEST(imu, interface_set_accelerometer_sensitivity_roughest){
-    std::unique_ptr<i2c::I2CHandler> i2c_handler = std::make_unique<i2c::I2CHandler>();
-    imu::InertialMeasurement imu_interface(std::move(i2c_handler));
+  TEST_F(ImuInterfaceTests, interface_set_accelerometer_sensitivity_roughest){
     types::AccelerometerSensitivity sensitivity = types::AccelerometerSensitivity::ROUGHEST;
-    imu_interface.SetAccelerometerSensitivity(sensitivity);
-    types::AccelerometerSensitivity gyroscope_sensitivity_return = imu_interface.GetAccelerometerSensitivity();
+    unit_under_test_->SetAccelerometerSensitivity(sensitivity);
+    types::AccelerometerSensitivity gyroscope_sensitivity_return = unit_under_test_->GetAccelerometerSensitivity();
     EXPECT_EQ(gyroscope_sensitivity_return, sensitivity);
   }
 
-  TEST(imu, interface_get_accelerometer_sensitivity_default){
-    std::unique_ptr<i2c::I2CHandler> i2c_handler = std::make_unique<i2c::I2CHandler>();
-    imu::InertialMeasurement imu_interface(std::move(i2c_handler));
+  TEST_F(ImuInterfaceTests, interface_get_accelerometer_sensitivity_default){
     types::AccelerometerSensitivity sensitivity = types::AccelerometerSensitivity::FINEST;
-    types::AccelerometerSensitivity gyroscope_sensitivity_return = imu_interface.GetAccelerometerSensitivity();
+    types::AccelerometerSensitivity gyroscope_sensitivity_return = unit_under_test_->GetAccelerometerSensitivity();
     EXPECT_EQ(gyroscope_sensitivity_return, sensitivity);
   }
 
-  TEST(imu, interface_get_gyroscope){
-    std::unique_ptr<i2c::I2CHandler> i2c_handler = std::make_unique<i2c::I2CHandler>();
-    imu::InertialMeasurement imu_interface(std::move(i2c_handler));
+  TEST_F(ImuInterfaceTests, interface_get_gyroscope){
     types::EuclideanVector<float> expected_value{1.5, 2.5, 3.5};
-    types::EuclideanVector<float> gyroscope_return = imu_interface.GetGyroscope();
+    types::EuclideanVector<float> gyroscope_return = unit_under_test_->GetGyroscope();
     EXPECT_EQ(gyroscope_return.x, expected_value.x);
     EXPECT_EQ(gyroscope_return.y, expected_value.y);
     EXPECT_EQ(gyroscope_return.z, expected_value.z);
   }
 
-  TEST(imu, interface_get_accelerometer){
-    std::unique_ptr<i2c::I2CHandler> i2c_handler = std::make_unique<i2c::I2CHandler>();
-    imu::InertialMeasurement imu_interface(std::move(i2c_handler));
+  TEST_F(ImuInterfaceTests, interface_get_accelerometer){
     types::EuclideanVector<float> expected_value{4.5, 5.5, 6.5};
-    types::EuclideanVector<float> accelerometer_return = imu_interface.GetAccelerometer();
+    types::EuclideanVector<float> accelerometer_return = unit_under_test_->GetAccelerometer();
     EXPECT_EQ(accelerometer_return.x, expected_value.x);
     EXPECT_EQ(accelerometer_return.y, expected_value.y);
     EXPECT_EQ(accelerometer_return.z, expected_value.z);
   }
 
-  TEST(imu, interface_get_magnetometer){
-    std::unique_ptr<i2c::I2CHandler> i2c_handler = std::make_unique<i2c::I2CHandler>();
-    imu::InertialMeasurement imu_interface(std::move(i2c_handler));
+  TEST_F(ImuInterfaceTests, interface_get_magnetometer){
     types::EuclideanVector<float> expected_value{7.5, 8.5, 9.5};
-    types::EuclideanVector<float> magnetometer_return = imu_interface.GetMagnetometer();
+    types::EuclideanVector<float> magnetometer_return = unit_under_test_->GetMagnetometer();
     EXPECT_EQ(magnetometer_return.x, expected_value.x);
     EXPECT_EQ(magnetometer_return.y, expected_value.y);
     EXPECT_EQ(magnetometer_return.z, expected_value.z);
   }
 
-  TEST(imu, interface_get_temperature){
-    std::unique_ptr<i2c::I2CHandler> i2c_handler = std::make_unique<i2c::I2CHandler>();
-    imu::InertialMeasurement imu_interface(std::move(i2c_handler));
+  TEST_F(ImuInterfaceTests, interface_get_temperature){
     int expected_value = 17;
-    int temperature_return = imu_interface.GetTemperature();
+    int temperature_return = unit_under_test_->GetTemperature();
     EXPECT_EQ(temperature_return, expected_value);
   }
 }
