716955f7bfd2a09dec0d1d9d9bfb55656ed546da
Aron <aron.heck@gmx.de>
2020-05-01 14:24:24 +0200

    fixed doc style

--- squidDrone/src/propulsion/esc.hpp
+++ squidDrone/src/propulsion/esc.hpp
@@ -7,60 +7,68 @@
 namespace propulsion{
 
   /**
-  * @class Esc 
-  * @brief The abstract Esc provides a interface for concrete Esc's to implement
-  * The Esc abstract interface dictates the methods which all concrete Esc's have to
-  * implement. 
-  */
+   * @class Esc 
+   * @brief The abstract Esc provides a interface for concrete Esc's to implement
+   * The Esc abstract interface dictates the methods which all concrete Esc's have to
+   * implement. 
+   * 
+   */
   class Esc{
     public:
       /**
-      * @brief  Default constructor may not be used, so
-      *         it is deleted.
-      */
+       * @brief  Default constructor may not be used, so
+       *         it is deleted.
+       * 
+       */
       Esc() = delete;
 
       /**
-      * @brief  The custom constructor is
-      *         the one to be used. 
-      * @param  timer The timer handle struct defined by hal driver 
-      *               stm32g4xx_hal_tim.h. 
-      * @param  channel The used channel for this timer
-      * The only thing this constructor does, is to set the protected 
-      * member variables timer_ and channel_.
-      */
+       * @brief  The custom constructor is
+       *         the one to be used. 
+       * @param  timer The timer handle struct defined by hal driver 
+       *               stm32g4xx_hal_tim.h. 
+       * @param  channel The used channel for this timer
+       * The only thing this constructor does, is to set the protected 
+       * member variables timer_ and channel_.
+       * 
+       */
       explicit Esc(TIM_HandleTypeDef* timer, std::uint32_t channel): timer_(timer), channel_(channel){};
 
       /**
-      * @brief The default destructor is sufficent
-      */
+       * @brief The default destructor is sufficent
+       * 
+       */
       virtual ~Esc() = default;
 
       /**
-      * @brief Abstract method for returning the maximum pulse duration for the concrete ESC
-      * @return Pulse duration in microseconds. 
-      */
+       * @brief Abstract method for returning the maximum pulse duration for the concrete ESC
+       * @return Pulse duration in microseconds. 
+       * 
+       */
       virtual const auto GetMaxPulseDurationInMicroSeconds() const noexcept -> int = 0;
 
       /**
-      * @brief Abstract method for returning the minimum pulse duration for the concrete ESC
-      * @return Pulse duration in microseconds. 
-      */
+       * @brief Abstract method for returning the minimum pulse duration for the concrete ESC
+       * @return Pulse duration in microseconds. 
+       * 
+       */
       virtual const auto GetMinPulseDurationInMicroSeconds() const noexcept -> int = 0;
 
       /**
-      * @brief Abstract method for setting the pulse duration based on the min and max values
-      * @param pulse_duration Desired Pulse duration in microseconds
-      * @param repetition_period Desired time until pulse is repeated
-      * @return A types::HalError type confirmation whether it was working or not
-      */
+       * @brief Abstract method for setting the pulse duration based on the min and max values
+       * @param pulse_duration Desired Pulse duration in microseconds
+       * @param repetition_period Desired time until pulse is repeated
+       * @return A types::HalError type confirmation whether it was working or not
+       * 
+       */
       virtual const auto SetPulseDuration(int pulse_duration, int repetition_period) noexcept -> types::HalError = 0;
 
     protected:
       /**
-      * Hal provided timer interface is protected, so
-      * the concrete implementation can use it.
-      */
+       * Hal provided timer interface is protected, so
+       * the concrete implementation can use it.
+       * 
+       */
       TIM_HandleTypeDef* timer_;
 
       /// The used channel number as can be seen stm32g4xx_hal_tim.h
--- squidDrone/src/propulsion/letodar_2204.hpp
+++ squidDrone/src/propulsion/letodar_2204.hpp
@@ -6,34 +6,39 @@
 namespace propulsion{
   
   /**
-  * @class LeTodar2204
-  * @brief The concrete implementation of the LeTodar2204 Motor
-  */
+   * @class LeTodar2204
+   * @brief The concrete implementation of the LeTodar2204 Motor
+   * 
+   */
   class LeTodar2204 final: public Motor{
     public:
       /**
-      * @brief Default ctor not needed
-      */
+       * @brief Default ctor not needed
+       * 
+       */
       LeTodar2204() = delete;
       
       /**
-      * @brief Custom ctor by using a pointer to a concrete Esc, identified by its abstract interface
-      * @param esc A unique pointer to a instance of a ESC
-      */
+       * @brief Custom ctor by using a pointer to a concrete Esc, identified by its abstract interface
+       * @param esc A unique pointer to a instance of a ESC
+       * 
+       */
       explicit LeTodar2204(std::unique_ptr<Esc> esc): Motor(std::move(esc)), speed_(0.0){}
 
       /**
-      * @brief Returns the last known speed in percent
-      */
+       * @brief Returns the last known speed in percent
+       * 
+       */
       auto GetCurrentSpeedInPercent() const noexcept -> const float override;
 
       /**
-      * @brief Sets the speed to the ESC based on the values configured there
-      * @param speed The speed to be set (between 0.0 and 100.0)
-      * @return types::InputError::INPUT_CORRECT if everything is working fine.
-      *         types::InputError::INPUT_OUT_OF_RANGE if speed exceeds lower and upper limits
-      *         types::InputError::INPUT_FAULTY if input caused the HAL to fail and throw an error.
-      */
+       * @brief Sets the speed to the ESC based on the values configured there
+       * @param speed The speed to be set (between 0.0 and 100.0)
+       * @return types::InputError::INPUT_CORRECT if everything is working fine.
+       *         types::InputError::INPUT_OUT_OF_RANGE if speed exceeds lower and upper limits
+       *         types::InputError::INPUT_FAULTY if input caused the HAL to fail and throw an error.
+       * 
+       */
       auto SetSpeedInPercent(const float speed) noexcept -> types::InputError override;
 
     private:
