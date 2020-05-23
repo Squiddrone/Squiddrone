c0977f8d54f8f70082c99ba32b31671e7c7bbc19
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-05-23 17:35:45 +0200

    Pass by const reference
    Codacy issue

--- squidDrone/src/i2c/i2c.cpp
+++ squidDrone/src/i2c/i2c.cpp
@@ -3,7 +3,7 @@
 namespace i2c
 {
 
-  auto I2C::read(uint8_t address, const std::vector<uint8_t> data, int timeout) noexcept -> std::vector<uint8_t> {
+  auto I2C::read(uint8_t address, const std::vector<uint8_t>& data, int timeout) noexcept -> std::vector<uint8_t> {
     auto result = std::vector<uint8_t>{0, 0, 0};
     return result;
 
@@ -11,7 +11,7 @@ namespace i2c
     HAL_StatusTypeDef ret = HAL_I2C_Master_Transmit(&hi2c2, address, buf, 1, 0);
   }
 
-  auto I2C::write(uint8_t address, const std::vector<uint8_t> data, int timeout) noexcept -> I2CStatus {
+  auto I2C::write(uint8_t address, const std::vector<uint8_t>& data, int timeout) noexcept -> I2CStatus {
     return I2CStatus::I2C_TRANSACTION_FAILED;
   }
 
