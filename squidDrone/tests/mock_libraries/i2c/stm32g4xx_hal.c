047366687d2e4395c46e394444faaac3a87df9bd
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-05-24 16:04:59 +0200

    style guide adaptions

--- squidDrone/tests/mock_libraries/i2c/stm32g4xx_hal.c
+++ squidDrone/tests/mock_libraries/i2c/stm32g4xx_hal.c
@@ -2,23 +2,20 @@
 
 HAL_StatusTypeDef HAL_I2C_Master_Receive(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout)
 {
-  if (DevAddress == 0x01)
-  {
+  if (DevAddress == 0x01) {
     pData[0] = 1;
     pData[1] = 2;
     pData[2] = 3;
+    pData[3] = 4;
     return HAL_OK;
   }
-  else if (DevAddress == 0x02)
-  {
+  else if (DevAddress == 0x02) {
     return HAL_ERROR;
   }
-  else if (DevAddress == 0x03)
-  {
+  else if (DevAddress == 0x03) {
     return HAL_TIMEOUT;
   }
-  else if (DevAddress == 0x04)
-  {
+  else if (DevAddress == 0x04) {
     return HAL_BUSY;
   }
 
