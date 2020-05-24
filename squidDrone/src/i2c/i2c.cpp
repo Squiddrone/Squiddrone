fba1600e0bed4d2b236ccfaa21b3697c3c7dd7c9
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-05-24 15:49:07 +0200

    read interface: Removal of data, add of byte_size

--- squidDrone/src/i2c/i2c.cpp
+++ squidDrone/src/i2c/i2c.cpp
@@ -3,15 +3,17 @@
 namespace i2c
 {
 
-  auto I2C::read(uint8_t address, const std::vector<uint8_t>& data, int timeout) noexcept -> std::tuple<I2CStatus, std::vector<uint8_t>> {
-    auto result = std::vector<uint8_t>{0, 0, 0};
-    return {I2CStatus::I2C_TRANSACTION_FAILED, result};
+  auto I2C::read(uint8_t address, uint16_t byte_size, int timeout) noexcept -> std::tuple<I2CStatus, std::vector<uint8_t>> {
+    uint8_t buffer[byte_size];
 
-    uint8_t buf[12];
-    HAL_StatusTypeDef ret = HAL_I2C_Master_Transmit(&hi2c2, address, buf, 1, 0);
+    HAL_StatusTypeDef ret = HAL_I2C_Master_Receive(&hi2c2, address, buffer, byte_size, timeout);
+
+    std::vector<uint8_t> data(buffer, buffer+byte_size);
+    return {I2CStatus::I2C_TRANSACTION_FAILED, data};
   }
 
   auto I2C::write(uint8_t address, const std::vector<uint8_t>& data, int timeout) noexcept -> I2CStatus {
+    HAL_StatusTypeDef ret = HAL_I2C_Master_Transmit(&hi2c2, address, (uint8_t*)data.data(), (uint16_t)data.size(), timeout);
     return I2CStatus::I2C_TRANSACTION_FAILED;
   }
 
