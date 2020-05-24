736f159f38329ff764193526ab59b457862d914c
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-05-24 16:05:14 +0200

    Add of return states

--- squidDrone/src/i2c/i2c.cpp
+++ squidDrone/src/i2c/i2c.cpp
@@ -6,10 +6,28 @@ namespace i2c
   auto I2C::read(uint8_t address, uint16_t byte_size, int timeout) noexcept -> std::tuple<I2CStatus, std::vector<uint8_t>> {
     uint8_t buffer[byte_size];
 
-    HAL_StatusTypeDef ret = HAL_I2C_Master_Receive(&hi2c2, address, buffer, byte_size, timeout);
+    HAL_StatusTypeDef hal_status = HAL_I2C_Master_Receive(&hi2c2, address, buffer, byte_size, timeout);
 
     std::vector<uint8_t> data(buffer, buffer+byte_size);
-    return {I2CStatus::I2C_TRANSACTION_FAILED, data};
+
+    I2CStatus i2c_status;
+    if (hal_status == HAL_OK) {
+        i2c_status = I2CStatus::I2C_TRANSACTION_SUCCESSFUL;
+      }
+    else if (hal_status == HAL_ERROR) {
+        i2c_status = I2CStatus::I2C_TRANSACTION_FAILED;
+      }
+    else if (hal_status == HAL_TIMEOUT) {
+        i2c_status = I2CStatus::I2C_TRANSACTION_TIMEOUT;
+      }
+    else if (hal_status == HAL_BUSY) {
+        i2c_status = I2CStatus::I2C_TRANSACTION_BUSY;
+      }
+    else {
+      i2c_status = I2CStatus::I2C_TRANSACTION_FAILED;
+    }
+
+    return {i2c_status, data};
   }
 
   auto I2C::write(uint8_t address, const std::vector<uint8_t>& data, int timeout) noexcept -> I2CStatus {
