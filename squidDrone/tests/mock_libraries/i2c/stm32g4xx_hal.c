85cc59a84ea194e240554ad5c54c79138e0101aa
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-05-24 15:58:20 +0200

    Add of unittest data

--- squidDrone/tests/mock_libraries/i2c/stm32g4xx_hal.c
+++ squidDrone/tests/mock_libraries/i2c/stm32g4xx_hal.c
@@ -2,6 +2,26 @@
 
 HAL_StatusTypeDef HAL_I2C_Master_Receive(I2C_HandleTypeDef *hi2c, uint16_t DevAddress, uint8_t *pData, uint16_t Size, uint32_t Timeout)
 {
+  if (DevAddress == 0x01)
+  {
+    pData[0] = 1;
+    pData[1] = 2;
+    pData[2] = 3;
+    return HAL_OK;
+  }
+  else if (DevAddress == 0x02)
+  {
+    return HAL_ERROR;
+  }
+  else if (DevAddress == 0x03)
+  {
+    return HAL_TIMEOUT;
+  }
+  else if (DevAddress == 0x04)
+  {
+    return HAL_BUSY;
+  }
+
   return HAL_ERROR;
 }
 
