aafd8eae3c05afc39b259c70dd05401178f53020
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-05-24 11:07:40 +0200

    Add of state for BUSY

--- squidDrone/src/i2c/i2c_status.hpp
+++ squidDrone/src/i2c/i2c_status.hpp
@@ -6,7 +6,8 @@ namespace i2c {
   enum class I2CStatus: int{
     I2C_TRANSACTION_SUCCESSFUL,
     I2C_TRANSACTION_FAILED,
-    I2C_TRANSACTION_TIMEOUT
+    I2C_TRANSACTION_TIMEOUT,
+    I2C_TRANSACTION_BUSY
   };
 
 } // namespace i2c
