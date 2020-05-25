65bba9548b8c5c8eb1a947034c3a77b82d279493
Aron <aron.heck@gmx.de>
2020-05-25 18:35:59 +0200

    minimal work on unittests done

--- squidDrone/src/propulsion/motor_builder.hpp
+++ squidDrone/src/propulsion/motor_builder.hpp
@@ -12,9 +12,21 @@
 #endif
 
 namespace propulsion{
+
+  /**
+   * @brief A static class which only exists to build a correct Motor object
+   */
   class MotorBuilder{
     public:
+      /// Ctor is deleted, because it should not be instantiated
       MotorBuilder() = delete;
+
+      /**
+       * @brief Constructs a Motor object
+       * 
+       * @param motor_config The information by which it can be determined what Motor to build
+       * @return const std::unique_ptr<Motor> A pointer to said Motor. Nullptr on error
+       */
       static auto Create(propulsion::PropulsionHardwareConfig motor_config) -> const std::unique_ptr<Motor>;
 
 
--- squidDrone/tests/mock_libraries/propulsion/motor_mock.hpp
+++ squidDrone/tests/mock_libraries/propulsion/motor_mock.hpp
@@ -1,4 +1,16 @@
 #ifndef TESTS_MOCK_LIBRARIES_PROPULSION_MOTOR_MOCK_HPP_
 #define TESTS_MOCK_LIBRARIES_PROPULSION_MOTOR_MOCK_HPP_
 
+
+#include <memory>
+#include "esc_mock.hpp"
+
+namespace propulsion{
+  class Motor{
+    public:
+      Motor(std::unique_ptr<Esc> esc): esc_{std::move(esc)}{} 
+      std::unique_ptr<Esc> esc_;
+  };
+}
+
 #endif
