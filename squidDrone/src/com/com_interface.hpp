070fdb094c98670340876c511f794ed0d3ac3e61
c0nwild <42233292+c0nwild@users.noreply.github.com>
2020-05-26 06:55:15 +0200

    Added virtual destructor and corrected comments

--- squidDrone/src/com/com_interface.hpp
+++ squidDrone/src/com/com_interface.hpp
@@ -20,41 +20,47 @@ namespace com {
   class ComInterface {
     public:
       /** 
-      * @brief Default constructor may not be used.
-      * 
-      */
+       * @brief Default constructor may not be used.
+       * 
+       */
       ComInterface() = delete;
 
       /** 
-      * @brief Construct a new Com Interface object
-      * 
-      */
+       * @brief Construct a new Com Interface object
+       * 
+       */
       explicit ComInterface(std::unique_ptr<com::ComMessageBuffer> msg_buf) : msg_buffer(std::move(msg_buf)){};
 
+      /**
+       * @brief Virtual destruktor. 
+       *
+       */
+      virtual ~ComInterface();
+
       /** 
-      * @brief Get the data packet object
-      * 
-      * @return decltype(array<std::uint8_t, 32>) 
-      */
+       * @brief Get the data packet object
+       * 
+       * @return decltype(array<std::uint8_t, 32>) 
+       */
       virtual auto get_data_packet() const noexcept -> std::array<std::uint8_t, 32> = 0;
       
       /** 
-      * @brief Transmit dataframe of max length 32 byte via the drones' wifi interface.
-      * 
-      * @param target_id target_id The wifi id of the receiver.
-      * @param payload payload An array of bytes. Max length 32 bytes. Must conform to SquidDrone 
-      * com protocol specification.
-      * @return decltype(std::uint8_t) true in case of successful transmission, false in case something 
-      * went wrong.
-      */
+       * @brief Transmit dataframe of max length 32 byte via the drones' wifi interface.
+       * 
+       * @param target_id target_id The wifi id of the receiver.
+       * @param payload payload An array of bytes. Max length 32 bytes. Must conform to SquidDrone 
+       * com protocol specification.
+       * @return decltype(std::uint8_t) true in case of successful transmission, false in case something 
+       * went wrong.
+       */
       virtual auto put_data_packet(std::uint8_t target_id, std::array<std::uint8_t, 32> &payload) const noexcept
       -> std::uint8_t = 0;
       
     protected:
       /** 
-      * @brief Unique pointer to an instance of a message buffer queue.
-      * 
-      */
+       * @brief Unique pointer to an instance of a message buffer queue.
+       * 
+       */
       std::unique_ptr<com::ComMessageBuffer> msg_buffer;
   };
 }
