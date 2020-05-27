1fa8418e10dc6c785d92382a5c18e52fae1d1541
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-05-27 19:03:17 +0200

    Change of type for timeout to uint32_t

--- squidDrone/src/i2c/i2c_interface.hpp
+++ squidDrone/src/i2c/i2c_interface.hpp
@@ -15,8 +15,8 @@ namespace i2c {
       virtual ~I2CInterface() = default;
       explicit I2CInterface(void){};
 
-      virtual auto read(uint8_t address, uint16_t byte_size, int timeout=I2C_STANDARD_TIMEOUT) noexcept -> std::tuple<I2CStatus, std::vector<uint8_t>> = 0;
-      virtual auto write(uint8_t address, const std::vector<uint8_t>& data, int timeout=I2C_STANDARD_TIMEOUT) noexcept -> I2CStatus = 0;
+      virtual auto read(uint8_t address, uint16_t byte_size, uint32_t timeout=I2C_STANDARD_TIMEOUT) noexcept -> std::tuple<I2CStatus, std::vector<uint8_t>> = 0;
+      virtual auto write(uint8_t address, const std::vector<uint8_t>& data, uint32_t timeout=I2C_STANDARD_TIMEOUT) noexcept -> I2CStatus = 0;
   };
 
 } // namespace i2c
