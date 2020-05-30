9e4c3c26a5e0e9b9da95adffd564debeffc842ee
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-05-30 16:12:54 +0200

    Add of i2c test

--- squidDrone/src/main.cpp
+++ squidDrone/src/main.cpp
@@ -15,7 +15,10 @@
 #include "spi_config.h"
 #include "timer_config.h"
 #include "mcu_settings.h"
-
+#include "i2c.hpp"
+#include <memory>
+#include <string.h>
+#include <stdio.h>
 
 int main(){
   HAL_Init();
@@ -34,7 +37,41 @@ int main(){
   MX_TIM16_Init();
   MX_TIM17_Init();
 
-  while(1){}
+  static const uint8_t MPU9250_ADDR = 0x68;
+  static const uint8_t WHO_AM_I = 0x75;
+  std::unique_ptr<i2c::I2C> i2c_interface;
+  i2c_interface = std::make_unique<i2c::I2C>();
+
+  uint8_t buf[12];
+
+  while (1)
+    {
+      std::vector<uint8_t> data = {WHO_AM_I};
+      i2c::I2CStatus i2c_status;
+      std::vector<uint8_t> result_data;
+
+      i2c_status = i2c_interface->Write(MPU9250_ADDR, data);
+
+      if (i2c_status != i2c::I2CStatus::I2C_TRANSACTION_SUCCESSFUL) {
+        strcpy((char*)buf, "Error Tx\r\n");
+      }
+      else {
+        std::tie(i2c_status, result_data) = i2c_interface->Read(MPU9250_ADDR, 1);
+
+        if (i2c_status != i2c::I2CStatus::I2C_TRANSACTION_SUCCESSFUL) {
+        strcpy((char*)buf, "Error Rx\r\n");
+        }
+        else {
+          uint8_t mpu_address = result_data.at(0);
+          sprintf((char*)buf, "%02X\r\n", mpu_address);
+        }
+
+      }
+
+      HAL_UART_Transmit(&huart2, buf, (uint16_t)strlen((char*)buf), HAL_MAX_DELAY);
+
+      HAL_Delay(500);
+    }
   return 0;
 }
 
