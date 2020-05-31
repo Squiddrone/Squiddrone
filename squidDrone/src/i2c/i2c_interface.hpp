29856cf1f262ef9c48fe6a87d1780ae11a7daf76
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-05-31 12:40:28 +0200

    Add of further documentation

--- squidDrone/src/i2c/i2c_interface.hpp
+++ squidDrone/src/i2c/i2c_interface.hpp
@@ -21,8 +21,10 @@ namespace i2c {
        * @brief Reads data from I2C Bus
        * 
        * @param address The address of the I2C Bus participant
-       * @param byte_size Amount of Bytes to read
-       * @param timeout Timeout in milliseconds
+       * @param byte_size Amount of Bytes to read \n
+       *                  Allowed range of Bytes is between 1 and 32.
+       * @param timeout Timeout in milliseconds \n
+       *                Allowed range of Timeout is between 1 and HAL_MAX_DELAY (0xFFFFFFFF, see stm32g4xx_hal_def.h).
        * @return std::tuple<#I2CStatus, std::vector<uint8_t>> Status of I2C Interface and Data read as std::vector. Each element is a Byte read.
        */
       virtual auto Read(std::uint8_t address, std::uint16_t byte_size, std::uint32_t timeout=I2C_STANDARD_TIMEOUT_IN_MS) noexcept -> std::tuple<I2CStatus, std::vector<std::uint8_t>> = 0;
@@ -31,8 +33,10 @@ namespace i2c {
        * @brief Writes data to I2C participant
        * 
        * @param address The address of the I2C Bus participant
-       * @param data Data to write to I2C participant. std::vector with each element is one Byte.
-       * @param timeout Timeout in milliseconds
+       * @param data Data to write to I2C participant. std::vector with each element is one Byte. \n
+       *             Allowed range of Bytes is between 1 and 32.
+       * @param timeout Timeout in milliseconds \n
+       *                Allowed range of Timeout is between 1 and HAL_MAX_DELAY (0xFFFFFFFF, see stm32g4xx_hal_def.h).
        * @return #I2CStatus Status of I2C Interface
        */
       virtual auto Write(std::uint8_t address, const std::vector<std::uint8_t>& data, std::uint32_t timeout=I2C_STANDARD_TIMEOUT_IN_MS) noexcept -> I2CStatus = 0;
