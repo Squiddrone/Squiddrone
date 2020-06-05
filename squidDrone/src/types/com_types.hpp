52bdd17637a2b10a9dd5b5dc11e73136386e034b
c0nwild <conradwild@hotmail.com>
2020-06-05 07:43:16 +0200

    Formatting corrections Added max queue length constant added type alias for array type

--- squidDrone/src/types/com_types.hpp
+++ squidDrone/src/types/com_types.hpp
@@ -8,17 +8,23 @@ namespace types {
    * @brief Enum class to contain error codes for COM module.
    * 
    */
-  enum class ComError: std::int8_t {
-      /// No error
-      COM_OK,
-      /// Buffer overflow error
-      COM_BUFFER_OVERFLOW,
-      /// Buffer IO error i.e. faulty data in buffer
-      COM_BUFFER_IO_ERROR
+  enum class ComError : std::int8_t {
+    /// No error
+    COM_OK,
+    /// Buffer overflow error
+    COM_BUFFER_OVERFLOW,
+    /// Buffer IO error i.e. faulty data in buffer
+    COM_BUFFER_IO_ERROR
   };
 
-  // Fixed length for com message frames.
-  const std::uint8_t max_frame_length = 32;
+  /// Fixed depth for the com buffer.
+  static constexpr std::uint8_t max_queue_len = 5;
+
+  /// Fixed length for com message frames.
+  static constexpr std::uint8_t max_frame_length = 32;
+
+  /// Type alias for com message frame datatype
+  using com_msg_frame = std::array<std::uint8_t, max_frame_length>;
 }
 
 #endif
