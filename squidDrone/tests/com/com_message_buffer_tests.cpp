7ea3db6b9fbdbab2ea80772751e425c8e1162ff1
c0nwild <conradwild@hotmail.com>
2020-06-05 07:47:07 +0200

    Now using type alias for array type Now strictly using public member functions

--- squidDrone/tests/com/com_message_buffer_tests.cpp
+++ squidDrone/tests/com/com_message_buffer_tests.cpp
@@ -5,8 +5,6 @@ namespace{
   // Derived class for testing queue behaviour
   class ComMessageBufferTest: public com::ComMessageBuffer {
     public:
-      using com::ComMessageBuffer::max_queue_len;
-      using com::ComMessageBuffer::data_;
       ComMessageBufferTest(){};
   };
 
@@ -15,7 +13,7 @@ namespace{
       virtual void SetUp(){
         ref_data.fill(0xfa);
       }
-      std::array<std::uint8_t, 32> ref_data;
+      types::com_msg_frame ref_data;
   };
 }
 
@@ -27,8 +25,8 @@ TEST_F(ComMessageBufferTests, put_data){
 
 TEST_F(ComMessageBufferTests, put_data_buffer_overflow){
   auto com_buffer = std::make_unique<ComMessageBufferTest>();
-  for (int n = 0; n <= com_buffer->max_queue_len; n++){
-    com_buffer->data_.push(ref_data);
+  for (int n = 0; n <= types::max_queue_len; n++){
+    com_buffer->PutData(ref_data);
   }
   auto rv = com_buffer->PutData(ref_data);
   ASSERT_EQ(rv, types::ComError::COM_BUFFER_OVERFLOW);
@@ -36,14 +34,14 @@ TEST_F(ComMessageBufferTests, put_data_buffer_overflow){
 
 TEST_F(ComMessageBufferTests, get_data){
   auto com_buffer = std::make_unique<ComMessageBufferTest>();
-  com_buffer->data_.push(ref_data);
+  com_buffer->PutData(ref_data);
   auto retrieved_data = com_buffer->GetData();
   ASSERT_EQ(ref_data,retrieved_data);
 }
 
 TEST_F(ComMessageBufferTests, get_data_with_empty_queue){
   auto com_buffer = std::make_unique<com::ComMessageBuffer>();
-  std::array<uint8_t, 32> comp_data = {0};
+  types::com_msg_frame comp_data = {0};
   auto retrieved_data = com_buffer->GetData();
   ASSERT_EQ(comp_data,retrieved_data);
 }
