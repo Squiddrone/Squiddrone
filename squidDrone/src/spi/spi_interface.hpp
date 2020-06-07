7595784c871487d69beb67e27c2b502b520f5291
c0nwild <conradwild@hotmail.com>
2020-06-07 21:50:24 +0200

    spi_interface.hpp
    -added constructor / destructor
    -corrected doxygen documentation

--- squidDrone/src/spi/spi_interface.hpp
+++ squidDrone/src/spi/spi_interface.hpp
@@ -2,24 +2,36 @@
 #define SRC_SPI_SPI_INTERFACE_HPP_
 
 #include <memory>
+
 #include "spi_status.hpp"
 
 namespace spi {
-  /**
+/**
    * @brief Abstract interface class to access SPI peripheral.
    * 
    */
-  class SPI_Interface {
-    public:
-    /**
+class SPIInterface {
+ public:
+  /**
+     * @brief Construct a new spi interface object. Default constructor.
+     * 
+     */
+  explicit SPIInterface() = default;
+
+  /**
+     * @brief Destroy the spi interface object. Default destructor.
+     * 
+     */
+  virtual ~SPIInterface() = default;
+  /**
      * @brief Perform an SPI Tx/Rx transaction.
      * 
      * @param pRxData Pointer to buffer for received data.
      * @param pTxData Pointer to buffer holding the data to be transmitted.
-     * @return spi::SPIStatus See \ref SPIStatus for reference.
+     * @return spi::SPIStatus Status information about the success of the transmission. See \ref SPIStatus for details.
      */
-    virtual auto Transfer(std::unique_ptr<std::uint8_t> pRxData, std::unique_ptr<std::uint8_t> pTxData) noexcept -> spi::SPIStatus = 0;
-  };
+  virtual auto Transfer(std::unique_ptr<std::uint8_t> pRxData, std::unique_ptr<std::uint8_t> pTxData) noexcept -> spi::SPIStatus = 0;
+};
 }  // namespace spi
 
 #endif
