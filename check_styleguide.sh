dc4b1eec149e859dd519abd3ef1912b01aaabe02
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-06-30 17:40:10 +0200

    Add of bash parameter for hard reset of git to head
    Use it in CI.
    -r sets repo hard to HEAD.
    Without parameter, no reset is done.

--- check_styleguide.sh
+++ check_styleguide.sh
@@ -2,20 +2,31 @@
 
 set -euo pipefail
 
+GIT_RESET=false;
+while getopts r option
+do
+case "${option}"
+in
+r) GIT_RESET=true;;
+esac
+done
+
 find . \( -name '*.hpp' -o -name '*.cpp' \) -exec clang-format -i {} +
 
 not_correct_list=$(git ls-files -m)
 
-if [[ -z $not_correct_list ]]; then
+if [[ -z ${not_correct_list} ]]; then
   echo "No styleguide violation with clang-format found.";
   exit 0;
 else
   echo "Please check following files:";
-  for f in $not_correct_list; do
-    echo "$f"
-    git --no-pager diff "$f"
+  for f in ${not_correct_list}; do
+    echo "${f}"
+    git --no-pager diff "${f}"
   done
 fi
 
-git reset HEAD --hard
+if [ ${GIT_RESET} = true ]; then
+  git reset HEAD --hard;
+fi
 exit 1;
