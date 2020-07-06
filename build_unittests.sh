ebe34f381c82d5d84d2e5d47f0fae92abf9331db
elheck <aron.heck@gmx.de>
2020-07-06 21:37:04 +0200

    Update build_unittests.sh

--- build_unittests.sh
+++ build_unittests.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 mkdir -p build_unittests && cd build_unittests
-
+ 
 # Configure
 cmake ../ -DGOOGLE_TESTS=ON -DCODE_COVERAGE=ON -DCMAKE_TOOLCHAIN_FILE=../cmake/gcc_linux_toolchain.cmake
 # Build (for Make on Unix equivalent to `make -j $(nproc)`)
