2615dba17bade739943dd0af5e1cb559d4cf85b4
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-07-12 17:26:16 +0200

    Add of unittests

--- tests/imu/imu_gyroscope_tests.cpp
+++ tests/imu/imu_gyroscope_tests.cpp
@@ -26,8 +26,17 @@ TEST_F(GyroscopeTests, gyroscope_Update) {
   EXPECT_EQ(gyroscope_update_return, types::HalError::WORKING);
 }
 
-TEST_F(GyroscopeTests, gyroscope_Get) {
+TEST_F(GyroscopeTests, gyroscope_Get_without_Update_first) {
+  types::EuclideanVector<float> expected_value{0, 0, 0};
+  auto gyroscope_get_return = unit_under_test_->Get();
+  EXPECT_EQ(gyroscope_get_return.x, expected_value.x);
+  EXPECT_EQ(gyroscope_get_return.y, expected_value.y);
+  EXPECT_EQ(gyroscope_get_return.z, expected_value.z);
+}
+
+TEST_F(GyroscopeTests, gyroscope_Get_with_Update_first) {
   types::EuclideanVector<float> expected_value{1.5, 2.5, 3.5};
+  unit_under_test_->Update();
   auto gyroscope_get_return = unit_under_test_->Get();
   EXPECT_EQ(gyroscope_get_return.x, expected_value.x);
   EXPECT_EQ(gyroscope_get_return.y, expected_value.y);
