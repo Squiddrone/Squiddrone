3efc3e9ffac62e9b08e3d83ba4d2f4ba4324f6e3
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-08-15 20:01:40 +0200

    Fix: Order of Bytes was wrong

--- tests/mock_libraries/imu/i2c.cpp
+++ tests/mock_libraries/imu/i2c.cpp
@@ -17,7 +17,7 @@ auto I2C::Read(std::uint8_t address, std::uint16_t byte_size, std::uint32_t time
     }
     if (unit_test_data.at(0) == imu::GYRO_XOUT_H) {
       i2c_status = I2CStatus::I2C_TRANSACTION_SUCCESSFUL;
-      data = {15, 0, 25, 0, 35, 0};
+      data = {0, 15, 0, 25, 0, 35};
     }
     if (unit_test_data.at(0) == imu::GYRO_CONFIG) {
       i2c_status = I2CStatus::I2C_TRANSACTION_SUCCESSFUL;
