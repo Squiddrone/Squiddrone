67e63f5aedd87da3b0be9e4e75eb0352251f364e
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-08-30 16:28:09 +0200

    Add of sceleton for mpu9255

---
+++ src/imu/mpu9255.cpp
@@ -0,0 +1,40 @@
+#include "mpu9255.hpp"
+
+namespace imu {
+
+void Mpu9255::SetGyroscopeSensitivity(types::ImuSensitivity gyroscope_sensitivity) noexcept {
+  gyroscope_sensitivity_ = gyroscope_sensitivity;
+}
+
+auto Mpu9255::GetGyroscopeSensitivity(void) noexcept -> types::ImuSensitivity {
+  return gyroscope_sensitivity_;
+}
+
+void Mpu9255::SetAccelerometerSensitivity(types::ImuSensitivity accelerometer_sensitivity) noexcept {
+  accelerometer_sensitivity_ = accelerometer_sensitivity;
+}
+
+auto Mpu9255::GetAccelerometerSensitivity(void) noexcept -> types::ImuSensitivity {
+  return accelerometer_sensitivity_;
+}
+
+auto Mpu9255::GetGyroscope(void) noexcept -> types::EuclideanVector<float> {
+  types::EuclideanVector<float> value{1.5, 2.5, 3.5};
+  return value;
+}
+
+auto Mpu9255::GetAccelerometer(void) noexcept -> types::EuclideanVector<float> {
+  types::EuclideanVector<float> value{4.5, 5.5, 6.5};
+  return value;
+}
+
+auto Mpu9255::GetMagnetometer(void) noexcept -> types::EuclideanVector<float> {
+  types::EuclideanVector<float> value{7.5, 8.5, 9.5};
+  return value;
+}
+
+auto Mpu9255::GetTemperature(void) noexcept -> int {
+  return 17;
+}
+
+}  // namespace imu
