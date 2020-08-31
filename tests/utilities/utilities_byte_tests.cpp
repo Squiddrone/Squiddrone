a2a14fe678e05aefe31161652a1f42bb741d00ca
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-08-31 16:04:58 +0200

    Add of byte_initialize_with_number

--- tests/utilities/utilities_byte_tests.cpp
+++ tests/utilities/utilities_byte_tests.cpp
@@ -17,6 +17,36 @@ TEST_F(UtilityByteTests, byte_initialize_empty) {
   EXPECT_EQ(0, unit_under_test_->Get());
 }
 
+TEST_F(UtilityByteTests, byte_initialize_with_number_5) {
+  unit_under_test_ = std::make_unique<utilities::Byte>(5);
+
+  EXPECT_EQ(5, unit_under_test_->Get());
+}
+
+TEST_F(UtilityByteTests, byte_initialize_with_number_0) {
+  unit_under_test_ = std::make_unique<utilities::Byte>(0);
+
+  EXPECT_EQ(0, unit_under_test_->Get());
+}
+
+TEST_F(UtilityByteTests, byte_initialize_with_number_255) {
+  unit_under_test_ = std::make_unique<utilities::Byte>(255);
+
+  EXPECT_EQ(255, unit_under_test_->Get());
+}
+
+TEST_F(UtilityByteTests, byte_initialize_with_number_minus_1) {
+  unit_under_test_ = std::make_unique<utilities::Byte>(-1);
+
+  EXPECT_EQ(0, unit_under_test_->Get());
+}
+
+TEST_F(UtilityByteTests, byte_initialize_with_number_256) {
+  unit_under_test_ = std::make_unique<utilities::Byte>(256);
+
+  EXPECT_EQ(0, unit_under_test_->Get());
+}
+
 }  // namespace
 
 int main(int argc, char** argv) {
