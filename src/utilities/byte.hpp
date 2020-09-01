6aae271021ba19df405b2faad4ea201b49e48222
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2020-09-01 16:17:57 +0200

    Convert to header-only

--- src/utilities/byte.hpp
+++ src/utilities/byte.hpp
@@ -11,12 +11,23 @@ class Byte {
   ~Byte() = default;
 
   explicit Byte(std::uint8_t byte) : byte_(byte){};
-  auto Get(void) noexcept -> std::uint8_t;
-  auto SetBit(std::uint8_t bit_number_between_0_and_7) noexcept -> void;
-  auto ClearBit(std::uint8_t bit_number_between_0_and_7) noexcept -> void;
+
+  auto Get(void) noexcept -> std::uint8_t {
+    return byte_;
+  }
+
+  auto SetBit(std::uint8_t bit_number_between_0_and_7) noexcept -> void {
+    byte_ = SetBitInByte(byte_, bit_number_between_0_and_7);
+  }
+
+  auto ClearBit(std::uint8_t bit_number_between_0_and_7) noexcept -> void {
+    byte_ = static_cast<std::uint8_t>(byte_ & ~SetBitInByte(0, bit_number_between_0_and_7));
+  }
 
  private:
-  auto SetBitInByte(std::uint8_t byte, std::uint8_t bit_number_between_0_and_7) noexcept -> std::uint8_t;
+  auto SetBitInByte(std::uint8_t byte, std::uint8_t bit_number_between_0_and_7) noexcept -> std::uint8_t {
+    return static_cast<std::uint8_t>(byte | 1 << bit_number_between_0_and_7);
+  }
 
   std::uint8_t byte_ = 0;
 };
