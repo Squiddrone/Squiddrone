80e45aded9955cb1689256cbf6fc737cfe20e1dc
Conrad Wild <cwild@Conrads-MacBook-Pro.local>
2020-11-01 22:11:52 +0100

    Refactoring spi.hpp/cpp - moved include directives to header file - introduced private member for cs pin setting - added data struct for defining chip select GPIO - added doxygen documentation

--- src/spi/spi.hpp
+++ src/spi/spi.hpp
@@ -1,13 +1,31 @@
 #ifndef SRC_SPI_SPI_HPP_
 #define SRC_SPI_SPI_HPP_
 
+#include <array>
+#include <spi_types.hpp>
 #include "spi_interface.hpp"
+#include "stm32g4xx_hal.h"
+
+//preserve include order
+#include "spi_config.h"
 
 namespace spi {
 
 ///Maximum buffer size allowed for SPI Transaction
 static constexpr std::uint8_t SPI_TRANSACTION_BUFFER_SIZE_LIMIT = 64;
 static constexpr std::uint8_t SPI_HAL_TX_RX_TIMEOUT = 100;
+
+//Valid values for chip select pin setting
+enum class PinSetting : bool {
+  HIGH = true,
+  LOW = false
+};
+
+//Data structure to hold the chip select GPIO information
+struct CSPin {
+  GPIO_TypeDef *peripheral;
+  uint16_t gpio_pin;
+};
 
 /**
  * @brief Concrete implementation of the SPI interface.
@@ -16,15 +34,30 @@ static constexpr std::uint8_t SPI_HAL_TX_RX_TIMEOUT = 100;
 class SPI final : spi::SPIInterface {
  public:
   SPI() = delete;
-  explicit SPI(const std::uint16_t chip_select) : spi::SPIInterface(), chip_select_(chip_select){};
+  explicit SPI(const CSPin chip_select) : spi::SPIInterface(), chip_select_(chip_select){};
   virtual ~SPI() = default;
 
   auto Transfer(std::vector<uint8_t> &miso_data_buffer, std::vector<uint8_t> &mosi_data_buffer) noexcept -> spi::SPIStatus override;
 
  private:
-  std::uint16_t chip_select_;
+  CSPin chip_select_;
 
-  auto IsBufferSizeExceedingLimits(std::uint16_t buffer_size) -> bool;
+  /**
+   * @brief Check if buffer size is exceeding maximuzm size.
+   * 
+   * @param buffer_size Buffer size in bytes.
+   * @return true Buffer size is exceeding limits.
+   * @return false Buffer size is within limits.
+   */
+  auto IsBufferSizeExceedingLimits(std::uint8_t buffer_size) noexcept -> bool;
+
+  /**
+   * @brief Set the Chip select GPIO pin.
+   * 
+   * @param pin_high_or_low The desired pin state. High or low.
+   * @return void
+   */
+  auto SetChipSelectPin(PinSetting pin_high_or_low) noexcept -> void;
 };
 
 }  // namespace spi
