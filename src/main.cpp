bcaa65fbd0ce449ca065d1a7cee487a715c12c88
c0nwild <42233292+c0nwild@users.noreply.github.com>
2020-11-03 18:46:35 +0100

    Update main.cpp
    Removal of lines for testing purposes

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@
 //
 #include "clock_config.h"
 #include "com/com_message_buffer.hpp"
-#include "com/nrf24l01.hpp"
 #include "cordic_config.h"
 #include "crc_config.h"
 #include "fmac_config.h"
@@ -37,15 +36,7 @@ int main() {
   MX_TIM16_Init();
   MX_TIM17_Init();
 
-  auto com_spi_handle = std::make_unique<spi::SPI>(CSCOM_Pin);
-  auto com_msg_buffer = std::make_unique<com::ComMessageBuffer>();
-  auto comsys = std::make_unique<com::NRF24L01>(std::move(com_spi_handle), std::move(com_msg_buffer));
-
-  types::com_msg_frame test_payload = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32};
-
   while (1) {
-    comsys->PutDataPacket(0x15, test_payload);
-    HAL_Delay(1000);
   }
   return 0;
 }
