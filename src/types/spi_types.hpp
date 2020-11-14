ce5d2f9becc40acbf9f26db6d56dbfa59255ed2f
Conrad Wild <cwild@Conrads-MacBook-Pro.local>
2020-11-14 21:48:31 +0100

    Changed spi_types.hpp
    - now contains SPIStatus enum class

--- src/types/spi_types.hpp
+++ src/types/spi_types.hpp
@@ -4,7 +4,20 @@
 #include <cinttypes>
 
 namespace types {
-
+/**
+ * @brief Enum class of type inf for spi status return values.
+ * 
+ */
+enum class SPIStatus : int {
+  ///SPI transaction was successful.
+  SPI_TRANSACTION_SUCCESSFUL,
+  ///SPI transaction failed.
+  SPI_TRANSACTION_FAILED,
+  ///Hal signaled an error.
+  SPI_HAL_ERROR,
+  ///Data buffer size exceeding limit
+  SPI_BUFFER_SIZE_LIMIT_EXCEEDED
+};
 }  // namespace types
 
 #endif
