4dac2f2f0e2949ef4adb07d1926193ee60414a3f
Conrad Wild <cwild@Conrads-MacBook-Pro.local>
2020-11-14 22:09:11 +0100

    Changed spi.cpp
    - adapted namespace of SPIStatus type
    - refactored private methods

--- src/spi/spi.cpp
+++ src/spi/spi.cpp
@@ -1,14 +1,14 @@
 #include "spi.hpp"
 
 namespace spi {
-auto SPI::Transfer(std::vector<uint8_t> &miso_data_buffer, std::vector<uint8_t> &mosi_data_buffer) noexcept -> spi::SPIStatus {
+auto SPI::Transfer(std::vector<uint8_t> &miso_data_buffer, std::vector<uint8_t> &mosi_data_buffer) noexcept -> types::SPIStatus {
   HAL_StatusTypeDef transmit_receive_ret_value = HAL_ERROR;
-  spi::SPIStatus return_value = spi::SPIStatus::SPI_HAL_ERROR;
+  types::SPIStatus return_value = types::SPIStatus::SPI_HAL_ERROR;
 
   uint8_t transmit_size = static_cast<uint8_t>(mosi_data_buffer.size());
 
   if (IsBufferSizeExceedingLimits(transmit_size)) {
-    return spi::SPIStatus::SPI_BUFFER_SIZE_LIMIT_EXCEEDED;
+    return types::SPIStatus::SPI_BUFFER_SIZE_LIMIT_EXCEEDED;
   }
 
   miso_data_buffer.resize(transmit_size);
@@ -24,35 +24,26 @@ auto SPI::Transfer(std::vector<uint8_t> &miso_data_buffer, std::vector<uint8_t> 
   SetChipSelectPin(PinSetting::LOW);
 
   if (transmit_receive_ret_value == HAL_OK) {
-    return_value = spi::SPIStatus::SPI_TRANSACTION_SUCCESSFUL;
+    return_value = types::SPIStatus::SPI_TRANSACTION_SUCCESSFUL;
   }
   if (transmit_receive_ret_value == HAL_TIMEOUT) {
-    return_value = spi::SPIStatus::SPI_TRANSACTION_FAILED;
+    return_value = types::SPIStatus::SPI_TRANSACTION_FAILED;
   }
   if (transmit_receive_ret_value == HAL_ERROR) {
-    return_value = spi::SPIStatus::SPI_HAL_ERROR;
+    return_value = types::SPIStatus::SPI_HAL_ERROR;
   }
   return return_value;
 }
 
 auto SPI::IsBufferSizeExceedingLimits(std::uint8_t buffer_size) noexcept -> bool {
-  bool return_value = false;
-  if (buffer_size > SPI_TRANSACTION_BUFFER_SIZE_LIMIT) {
-    return_value = true;
-  }
-  return return_value;
+  return buffer_size > SPI_TRANSACTION_BUFFER_SIZE_LIMIT;
 }
 
-auto SPI::SetChipSelectPin(PinSetting pin_high_or_low) noexcept -> void {
+auto SPI::SetChipSelectPin(PinSetting pin_setting) noexcept -> void {
   GPIO_PinState pin_state = GPIO_PIN_RESET;
 
-  switch (pin_high_or_low) {
-    case PinSetting::HIGH:
-      pin_state = GPIO_PIN_SET;
-      break;
-    case PinSetting::LOW:
-      pin_state = GPIO_PIN_RESET;
-      break;
+  if (pin_setting == PinSetting::HIGH) {
+    pin_state = GPIO_PIN_SET;
   }
 
   HAL_GPIO_WritePin(chip_select_.peripheral, chip_select_.gpio_pin, pin_state);
