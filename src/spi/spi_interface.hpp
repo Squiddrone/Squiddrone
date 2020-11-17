0a1d90623f0f96c3657aa26be2674a938f1c09ee
Conrad Wild <cwild@Conrads-MacBook-Pro.local>
2020-11-17 22:41:46 +0100

    Changes to interface.hpp
    -MOSI data buffer can be any length
    -MISO data buffer must be at least size of MOSI buffer
    -Transfer() return value is now of type DriverStatus

--- src/spi/spi_interface.hpp
+++ src/spi/spi_interface.hpp
@@ -4,6 +4,7 @@
 #include <cstdint>
 #include <vector>
 
+#include <error_types.hpp>
 #include <spi_types.hpp>
 
 namespace spi {
@@ -28,13 +29,16 @@ class SPIInterface {
   /**
    * @brief Perform an SPI Tx/Rx transaction. The transaction is bidirectional. While MOSI data are transmitted, the incoming MISO data are 
    * stored in the miso_data_buffer. The maximum length of any SPI transaction is limited to a maximum length defined in
-   * SPI_TRANSACTION_BUFFER_SIZE_LIMIT. Transaction buffer size for both buffers is determined by the MOSI buffer.
-   * 
-   * @param miso_data_buffer Reference to buffer for received data. Has to be provided by the caller.
+   * SPI_TRANSACTION_LENGTH_LIMIT.
    * @param mosi_data_buffer Reference to buffer holding the data to be transmitted. Has to be provided by the caller.
-   * @return spi::SPIStatus Status information about the success of the transmission. See SPIStatus for details.
+   * @param miso_data_buffer Reference to buffer for received data. Has to be provided by the caller. Must be at least as big as mosi_data_buffer.
+   * @return types::DriverStatus Status information about the success of the transmission. Possible values: 
+   * -OK - Transaction finished without errors.
+   * -HAL_ERROR - HAL function returned an error status
+   * -INPUT_ERROR - Maximum transaction length exceeded or miso buffer too small
+   * -TIMEOUT - HAL function returns timeout.
    */
-  virtual auto Transfer(std::vector<std::uint8_t> &miso_data_buffer, std::vector<std::uint8_t> &mosi_data_buffer) noexcept -> types::SPIStatus = 0;
+  virtual auto Transfer(std::vector<std::uint8_t> &mosi_data_buffer, std::vector<std::uint8_t> &miso_data_buffer) noexcept -> types::DriverStatus = 0;
 };
 }  // namespace spi
 
