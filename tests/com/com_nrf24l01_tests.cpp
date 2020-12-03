31bd6f0cb2693bb17caecf209d0d70d15d69a527
Conrad Wild <cwild@Conrads-MacBook-Pro.local>
2020-12-03 20:07:36 +0100

    test definitions for NRF24L01 class

---
+++ tests/com/com_nrf24l01_tests.cpp
@@ -0,0 +1,11 @@
+#include "com_nrf24l01.hpp"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace {
+class ComNRF24L01Tests : public ::testing::Test {
+ protected:
+  virtual void SetUp() {
+  }
+};
+}  // namespace
