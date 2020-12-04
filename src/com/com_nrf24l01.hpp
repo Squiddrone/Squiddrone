3ce854d5d2b44097de8e13996670ce7438f4df38
c0nwild <conradwild@hotmail.com>
2020-12-04 22:50:04 +0100

    added some private functions

--- src/com/com_nrf24l01.hpp
+++ src/com/com_nrf24l01.hpp
@@ -2,6 +2,7 @@
 #define SRC_COM_COM_NRF24L01_HPP_
 
 #include "com_interface.hpp"
+#include "com_nrf24l01_reg.hpp"
 
 namespace com {
 
@@ -18,6 +19,14 @@ class NRF24L01 final : public ComInterface {
   explicit NRF24L01(std::unique_ptr<com::ComMessageBuffer> msg_buf) : ComInterface(std::move(msg_buf)){};
   NRF24L01() = delete;
   ~NRF24L01() = default;
+
+ private:
+  auto SetConfiguration(std::uint8_t register_addr, std::uint8_t byte_offset, std::uint8_t value) noexcept -> types::DriverStatus;
+  auto GetConfiguration(std::uint8_t register_addr) noexcept -> std::uint8_t;
+  auto SetRXAddress(std::uint8_t data_pipe, std::uint_fast64_t address) noexcept -> types::DriverStatus;
+  auto SetTXAddress(std::uint_fast64_t address) noexcept -> types::DriverStatus;
+
+  ComMessageBuffer msg_buffer;
 };
 }  // namespace com
 
