e5b1e6bb4b517df1a92aa2ef8595e88c7b73e6ef
elheck <aron.heck@gmx.de>
2020-12-10 15:03:29 +0100

    Mock returning unique_ptr is fixed now

--- tests/propulsion/motor_driver_test.cpp
+++ tests/propulsion/motor_driver_test.cpp
@@ -6,19 +6,19 @@
 #include "motor.hpp"
 #include "motor_builder_mock.hpp"
 
-using ::testing::ByMove;
 using ::testing::NiceMock;
-using ::testing::Return;
 
 namespace {
 class MotorDriverTest : public ::testing::Test {
  protected:
   virtual void SetUp() override {
-    ON_CALL(*builder_, Create).WillByDefault(Return(ByMove(std::make_unique<propulsion::MotorMock>())));
+    ON_CALL(*builder_, Create).WillByDefault([](propulsion::PropulsionHardwareConfig &) {
+      return std::make_unique<propulsion::MotorMock>();
+    });
     unit_under_test_ = std::make_unique<propulsion::MotorDriver>(std::move(builder_));
   }
 
-  std::unique_ptr<propulsion::MotorBuilder> builder_;
+  std::unique_ptr<NiceMock<MockMotorBuilder>> builder_ = std::make_unique<NiceMock<MockMotorBuilder>>();
   std::unique_ptr<propulsion::MotorDriver> unit_under_test_;
 };
 
