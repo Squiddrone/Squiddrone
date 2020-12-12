f2c9646374f3c6dd99495693c6c46303f2479b8b
Conrad Wild <cwild@Conrads-MacBook-Pro.local>
2020-12-12 21:01:33 +0100

    added test frame and mocks

--- tests/com/com_nrf24l01_tests.cpp
+++ tests/com/com_nrf24l01_tests.cpp
@@ -7,5 +7,14 @@ class ComNRF24L01Tests : public ::testing::Test {
  protected:
   virtual void SetUp() {
   }
+
+  std::unique_ptr<com::ComMessageBuffer> com_msg_buffer_;
+  spi::CSPinDefinition com_cs_pin;
 };
+
+TEST_F(ComNRF24L01Tests, some_test) {
+  auto nrf24l01 = std::make_unique<com::NRF24L01>(std::move(com_msg_buffer_), com_cs_pin);
+  ASSERT_TRUE(true);
+}
+
 }  // namespace
