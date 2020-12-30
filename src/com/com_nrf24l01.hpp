0a621d7efcdcda27e767393c25fcdb4ef9edf8ff
c0nwild <conradwild@hotmail.com>
2020-12-30 14:23:17 +0100

    added a bunch of function definitions

--- src/com/com_nrf24l01.hpp
+++ src/com/com_nrf24l01.hpp
@@ -14,7 +14,7 @@ namespace com {
 class NRF24L01 final : public ComInterface {
  public:
   auto GetDataPacket() const noexcept -> types::com_msg_frame override;
-  auto PutDataPacket(std::uint8_t target_id, types::com_msg_frame &payload) const noexcept
+  auto PutDataPacket(std::uint8_t target_id, types::com_msg_frame &payload) noexcept
       -> types::ComError override;
 
   explicit NRF24L01(std::unique_ptr<com::ComMessageBuffer> msg_buf, spi::CSPinDefinition &cs_pin) : ComInterface(std::move(msg_buf)),
@@ -25,18 +25,52 @@ class NRF24L01 final : public ComInterface {
  private:
   spi::CSPin cs_pin_;
   spi::SPI spi_{cs_pin_};
-  auto SetConfigBit(std::uint8_t register_addr, std::uint8_t bit_value) const noexcept -> void;
+
+  //Pipe configuration
+  auto EnableDataPipe(DataPipe pipe_no) const noexcept -> types::DriverStatus;
+  auto DisableDataPipe(DataPipe pipe_no) const noexcept -> types::DriverStatus;
+  /**
+   * @brief Set the Tx Address and configure Pipe 0 for ack reception.
+   * 
+   * @param tx_addr 
+   * @return types::DriverStatus 
+   */
+  auto SetTxAddress(data_pipe_address tx_addr) const noexcept -> types::DriverStatus;
+  auto SetRxAddress(DataPipe pipe_no, data_pipe_address rx_addr) const noexcept -> types::DriverStatus;
+  /**
+   * @brief Set the Rx Payload Size per data pipe.
+   * 
+   * @param pipe_no 
+   * @param payload_size Payload size in bytes. 0-32 bytes allowed.
+   * @return types::DriverStatus 
+   */
+  auto SetRxPayloadSize(DataPipe pipe_no, std::size_t payload_size) const noexcept -> types::DriverStatus;
+  auto EnableAutoAck(DataPipe pipe_no) const noexcept -> types::DriverStatus;
+
+  // Transceiver hardware configuration
   auto EnableTxMode() const noexcept -> types::DriverStatus;
   auto EnableRxMode() const noexcept -> types::DriverStatus;
-  auto SetTxAddress(std::uint64_t tx_addr) const noexcept -> types::DriverStatus;
+  auto SetPowerState(State power_state) const noexcept -> types::DriverStatus;
+  auto SetRFChannel(std::uint8_t channel) const noexcept -> types::DriverStatus;
+  auto SetDataRate(DataRateSetting data_rate) const noexcept -> types::DriverStatus;
+  auto SetRFOutputPower(RFPowerSetting rf_power) const noexcept -> types::DriverStatus;
+  // Not sure if we ever need this
+  auto SetLNAGain(State state) const noexcept -> types::DriverStatus;
+
+  // General protocol configuration
   auto SetPayloadData() const noexcept -> types::DriverStatus;
-  auto EnableRxAddress(DataPipe pipe_no) const noexcept -> types::DriverStatus;
-  auto SetRxAddress(DataPipe pipe_no, std::uint64_t rx_addr) const noexcept -> types::DriverStatus;
-  auto EnableAutoAck(DataPipe pipe_no) const noexcept -> types::DriverStatus;
+  auto SetAddressWidth(DataPipeAddressWidth address_width) const noexcept -> types::DriverStatus;
   auto EnableCRC() const noexcept -> types::DriverStatus;
   auto SetCRCEncodingScheme(CRCEncodingScheme encoding_scheme) const noexcept -> types::DriverStatus;
-  auto PowerUp() const noexcept -> types::DriverStatus;
-  auto PowerDown() const noexcept -> types::DriverStatus;
+  auto ConfigAutoRetransmission(AutoRetransmissionDelay delay, AutoRetransmitCount count)
+      const noexcept -> types::DriverStatus;
+
+  // Status register access
+  auto CarrierDetected() const noexcept -> bool;
+
+  // Helper functions
+  auto ReadRegister(std::uint8_t register_address) noexcept -> std::uint8_t;
+  auto WriteRegister(std::uint8_t register_address, std::uint8_t register_content) -> types::DriverStatus;
 };
 }  // namespace com
 
