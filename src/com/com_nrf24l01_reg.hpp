d2cc3cffb03d8b583a52c0260c83d2a86fb24be9
c0nwild <conradwild@hotmail.com>
2020-12-30 14:23:47 +0100

    added types and register definitions

--- src/com/com_nrf24l01_reg.hpp
+++ src/com/com_nrf24l01_reg.hpp
@@ -1,9 +1,15 @@
 #ifndef SRC_COM_COM_NRF24L01_REG_HPP_
 #define SRC_COM_COM_NRF24L01_REG_HPP_
 
+#include <array>
 #include <cstdint>
 
 namespace com {
+
+enum class State : std::uint8_t {
+  disabled = 0,
+  enabled
+};
 
 enum class CRCEncodingScheme : std::uint8_t {
   one_byte,
@@ -11,13 +17,84 @@ enum class CRCEncodingScheme : std::uint8_t {
 };
 
 enum class DataPipe : std::uint8_t {
-  data_pipe_0,
+  data_pipe_0 = 0,
   data_pipe_1,
   data_pipe_2,
   data_pipe_3,
   data_pipe_4,
   data_pipe_5
 };
+
+enum class DataPipeAddressWidth : std::uint8_t {
+  aw_3bytes = 0x1,
+  aw_4bytes = 0x2,
+  aw_5bytes = 0x3,
+};
+
+enum class AutoRetransmissionDelay : std::uint8_t {
+  // auto retransmis delay 250-4000 us
+  ard250us = 0x00,
+  ard500us = 0x10,
+  ard750us = 0x20,
+  ard1000us = 0x30,
+  ard1250us = 0x40,
+  ard1500us = 0x50,
+  ard1750us = 0x60,
+  ard2000us = 0x70,
+  ard2250us = 0x80,
+  ard2500us = 0x90,
+  ard2750us = 0xa0,
+  ard3000us = 0xb0,
+  ard3250us = 0xc0,
+  ard3500us = 0xd0,
+  ard3750us = 0xe0,
+  ard4000us = 0xf0
+};
+
+enum class AutoRetransmitCount : std::uint8_t {
+  // auto retransmit count 0-15 in case of failed AR
+  arc0 = 0x0,
+  arc1 = 0x1,
+  arc2 = 0x2,
+  arc3 = 0x3,
+  arc4 = 0x4,
+  arc5 = 0x5,
+  arc6 = 0x6,
+  arc7 = 0x7,
+  arc8 = 0x8,
+  arc9 = 0x9,
+  arc10 = 0xa,
+  arc11 = 0xb,
+  arc12 = 0xc,
+  arc13 = 0xd,
+  arc14 = 0xe,
+  arc15 = 0xf
+};
+
+enum class RFPowerSetting : std::uint8_t {
+  rf_pwr_18dBm = 0x0,
+  rf_pwr_12dBm = 0x1,
+  rf_pwr_6dBm = 0x2,
+  rf_pwr_0dBm = 0x3
+};
+
+enum class DataRateSetting : std::uint8_t {
+  rf_dr_1mbps = 0,
+  rf_dr_2mbps = 2
+};
+
+using data_pipe_address = std::array<std::uint8_t, 5>;
+
+namespace instruction_word {
+const std::uint8_t R_REGISTER = 0x00;
+const std::uint8_t W_REGISTER = 0x20;
+const std::uint8_t R_RX_PAYLOAD = 0x61;
+const std::uint8_t W_TX_PAYLOAD = 0xa0;
+const std::uint8_t FLUSH_TX = 0xe1;
+const std::uint8_t FLUSH_RX = 0xe2;
+const std::uint8_t REUSE_TX_PL = 0xe3;
+const std::uint8_t NOP = 0xff;
+}  // namespace instruction_word
 
 namespace reg {
 /// configuration register
@@ -57,67 +134,28 @@ const std::uint8_t ERX_P0 = 1;
 /// setup of address widths
 namespace setup_aw {
 const std::uint8_t REG_ADDR = 0x3;
-const std::uint8_t AW_3BYTES = 0x1;
-const std::uint8_t AW_4BYTES = 0x2;
-const std::uint8_t AW_5BYTES = 0x3;
 }  // namespace setup_aw
 
 /// setup of automatic retransmission
 namespace setup_retr {
 const std::uint8_t REG_ADDR = 0x4;
-// auto retransmis delay 250-4000 us
-const std::uint8_t ARD250 = 0x00;
-const std::uint8_t ARD500 = 0x10;
-const std::uint8_t ARD750 = 0x20;
-const std::uint8_t ARD1000 = 0x30;
-const std::uint8_t ARD1250 = 0x40;
-const std::uint8_t ARD1500 = 0x50;
-const std::uint8_t ARD1750 = 0x60;
-const std::uint8_t ARD2000 = 0x70;
-const std::uint8_t ARD2250 = 0x80;
-const std::uint8_t ARD2500 = 0x90;
-const std::uint8_t ARD2750 = 0xa0;
-const std::uint8_t ARD3000 = 0xb0;
-const std::uint8_t ARD3250 = 0xc0;
-const std::uint8_t ARD3500 = 0xd0;
-const std::uint8_t ARD3750 = 0xe0;
-const std::uint8_t ARD4000 = 0xf0;
-
-// auto retransmit count 0-15 in case of failed AR
-const std::uint8_t ARC0 = 0x0;
-const std::uint8_t ARC1 = 0x1;
-const std::uint8_t ARC2 = 0x2;
-const std::uint8_t ARC3 = 0x3;
-const std::uint8_t ARC4 = 0x4;
-const std::uint8_t ARC5 = 0x5;
-const std::uint8_t ARC6 = 0x6;
-const std::uint8_t ARC7 = 0x7;
-const std::uint8_t ARC8 = 0x8;
-const std::uint8_t ARC9 = 0x9;
-const std::uint8_t ARC10 = 0xa;
-const std::uint8_t ARC11 = 0xb;
-const std::uint8_t ARC12 = 0xc;
-const std::uint8_t ARC13 = 0xd;
-const std::uint8_t ARC14 = 0xe;
-const std::uint8_t ARC15 = 0xf;
 }  // namespace setup_retr
 
+/// RF channel setting
 namespace rf_ch {
 const std::uint8_t REG_ADDR = 0x5;
 }  // namespace rf_ch
 
+/// RF setup register
 namespace rf_setup {
 const std::uint8_t REG_ADDR = 0x6;
 const std::uint8_t PLL_LOCK = 4;
 const std::uint8_t RF_DR = 3;
 const std::uint8_t RF_PWR = 1;
-const std::uint8_t RF_PWR_18dBm = 0x0;
-const std::uint8_t RF_PWR_12dBm = 0x1;
-const std::uint8_t RF_PWR_6dBm = 0x2;
-const std::uint8_t RF_PWR_0dBm = 0x3;
 const std::uint8_t LNA_HCURR = 0;
 }  // namespace rf_setup
 
+/// Status Register
 namespace status {
 const std::uint8_t REG_ADDR = 0x7;
 const std::uint8_t RX_DR = 6;
@@ -134,69 +172,85 @@ const std::uint8_t RX_FIFO_EMPTY = 0b111;
 const std::uint8_t TX_FULL = 0;
 }  // namespace status
 
+/// Transmit observe register
 namespace observe_tx {
 const std::uint8_t REG_ADDR = 0x8;
 const std::uint8_t PLOS_CNT = 4;
 const std::uint8_t ARC_CNT = 0;
 }  // namespace observe_tx
 
+/// Carrier detect
 namespace cd {
 const std::uint8_t REG_ADDR = 0x9;
 const std::uint8_t CD = 0;
 }  // namespace cd
 
+/// Receive address data pipe 0. 5 Bytes maximum length.
 namespace rx_addr_p0 {
 const std::uint8_t REG_ADDR = 0x0a;
 }  // namespace rx_addr_p0
 
+/// Receive address data pipe 1. 5 Bytes maximum length.
 namespace rx_addr_p1 {
 const std::uint8_t REG_ADDR = 0x0b;
 }  // namespace rx_addr_p1
 
+/// Receive address data pipe 2. Only LSB.
 namespace rx_addr_p2 {
 const std::uint8_t REG_ADDR = 0x0c;
 }  // namespace rx_addr_p2
 
+/// Receive address data pipe 3. Only LSB.
 namespace rx_addr_p3 {
 const std::uint8_t REG_ADDR = 0x0d;
 }  // namespace rx_addr_p3
 
+/// Receive address data pipe 4. Only LSB.
 namespace rx_addr_p4 {
 const std::uint8_t REG_ADDR = 0x0e;
 }  // namespace rx_addr_p4
 
+/// Receive address data pipe 5. Only LSB.
 namespace rx_addr_p5 {
 const std::uint8_t REG_ADDR = 0x0f;
 }  // namespace rx_addr_p5
 
+/// Transmit address. Used for a PTX device only.
 namespace tx_addr {
 const std::uint8_t REG_ADDR = 0x10;
 }  // namespace tx_addr
 
+/// Number of bytes in RX payload in data pipe 0.
 namespace rx_pw_p0 {
 const std::uint8_t REG_ADDR = 0x11;
 }  // namespace rx_pw_p0
 
+/// Number of bytes in RX payload in data pipe 1.
 namespace rx_pw_p1 {
 const std::uint8_t REG_ADDR = 0x12;
 }  // namespace rx_pw_p1
 
+/// Number of bytes in RX payload in data pipe 2.
 namespace rx_pw_p2 {
 const std::uint8_t REG_ADDR = 0x13;
 }  // namespace rx_pw_p2
 
+/// Number of bytes in RX payload in data pipe 3.
 namespace rx_pw_p3 {
 const std::uint8_t REG_ADDR = 0x14;
 }  // namespace rx_pw_p3
 
+/// Number of bytes in RX payload in data pipe 4.
 namespace rx_pw_p4 {
 const std::uint8_t REG_ADDR = 0x15;
 }  // namespace rx_pw_p4
 
+/// Number of bytes in RX payload in data pipe 5.
 namespace rx_pw_p5 {
 const std::uint8_t REG_ADDR = 0x16;
 }  // namespace rx_pw_p5
 
+/// FIFO status register
 namespace fifo_status {
 const std::uint8_t REG_ADDR = 0x17;
 const std::uint8_t TX_REUSE = 6;
