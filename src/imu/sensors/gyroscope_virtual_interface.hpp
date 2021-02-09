d689a792fe6f395a6a51e39d3e3e099068f0f523
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2021-02-09 10:17:19 +0100

    Removal of codefactor issue

--- src/imu/sensors/gyroscope_virtual_interface.hpp
+++ src/imu/sensors/gyroscope_virtual_interface.hpp
@@ -11,7 +11,7 @@ class GyroscopeInterface : public InertialMeasurementSensorWithSensitivity {
   ~GyroscopeInterface() = default;
 
   explicit GyroscopeInterface(std::shared_ptr<i2c::I2CInterface> i2c_handler) : InertialMeasurementSensorWithSensitivity(i2c_handler){};
-  virtual auto Init(std::uint8_t i2c_address) noexcept -> types::DriverStatus override = 0;
+  virtual auto Init(std::uint8_t i2c_address) noexcept -> types::DriverStatus = 0;
 };
 
 }  // namespace imu
