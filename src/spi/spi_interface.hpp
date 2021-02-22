10d962b804a9225186a07329ae86fdbeb82e00f5
c0nwild <conradwild@hotmail.com>
2021-02-22 22:28:02 +0100

    Read makes no sense

--- src/spi/spi_interface.hpp
+++ src/spi/spi_interface.hpp
@@ -35,19 +35,12 @@ class SPIInterface {
   virtual auto Write(std::vector<std::uint8_t> &mosi_data_buffer) noexcept -> types::DriverStatus = 0;
 
   /**
-   * @brief Perform an SPI Read transaction.
-   * 
-   * @param miso_data_buffer Reference to buffer for data reception.
-   * @return types::DriverStatus Status information about the success of the transmission.
-   */
-  virtual auto Read(std::vector<std::uint8_t> &miso_data_buffer) noexcept -> types::DriverStatus = 0;
-
-  /**
    * @brief Perform an SPI Tx/Rx transaction. The transaction is bidirectional. While MOSI data are transmitted, the incoming MISO data are 
    * stored in the miso_data_buffer. The maximum length of any SPI transaction is limited to a maximum length defined in
    * SPI_TRANSACTION_LENGTH_LIMIT.
    * @param mosi_data_buffer Reference to buffer holding the data to be transmitted. Has to be provided by the caller.
    * @param miso_data_buffer Reference to buffer for received data. Has to be provided by the caller. Must be at least as big as mosi_data_buffer.
+   * In case of a read action, miso data buffer length can be used to force the transfer length.
    * @return types::DriverStatus Status information about the success of the transmission. Possible values: 
    * -OK - Transaction finished without errors.
    * -HAL_ERROR - HAL function returned an error status
