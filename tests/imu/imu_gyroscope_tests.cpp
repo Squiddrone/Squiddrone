b63250ead03dbabcbbe21ecca370fc63b51338ae
ThorsHamster <48162347+ThorsHamster@users.noreply.github.com>
2021-02-22 17:18:13 +0100

    Removal of unnecessary prefix

--- tests/imu/imu_gyroscope_tests.cpp
+++ tests/imu/imu_gyroscope_tests.cpp
@@ -50,64 +50,64 @@ class GyroscopeTests : public ::testing::Test {
       types::DriverStatus::HAL_ERROR, {}};
 };
 
-TEST_F(GyroscopeTests, gyroscope_Init_OK) {
+TEST_F(GyroscopeTests, Init_OK) {
   ConfigureUnitUnderTest();
 
-  auto gyroscope_init_return = unit_under_test_->Init(i2c_address_);
+  auto init_return = unit_under_test_->Init(i2c_address_);
 
-  EXPECT_EQ(gyroscope_init_return, types::DriverStatus::OK);
+  EXPECT_EQ(init_return, types::DriverStatus::OK);
 }
 
-TEST_F(GyroscopeTests, gyroscope_Init_failed) {
+TEST_F(GyroscopeTests, Init_failed) {
   ON_CALL(*i2c_handler_, Write(0, _, _))
       .WillByDefault(Return(types::DriverStatus::HAL_ERROR));
 
   ConfigureUnitUnderTest();
 
-  auto gyroscope_init_return = unit_under_test_->Init(0);
+  auto init_return = unit_under_test_->Init(0);
 
-  EXPECT_EQ(gyroscope_init_return, types::DriverStatus::HAL_ERROR);
+  EXPECT_EQ(init_return, types::DriverStatus::HAL_ERROR);
 }
 
-TEST_F(GyroscopeTests, gyroscope_Update) {
+TEST_F(GyroscopeTests, Update) {
   ConfigureUnitUnderTest();
 
   unit_under_test_->Init(i2c_address_);
-  auto gyroscope_update_return = unit_under_test_->Update();
-  EXPECT_EQ(gyroscope_update_return, types::DriverStatus::OK);
+  auto update_return = unit_under_test_->Update();
+  EXPECT_EQ(update_return, types::DriverStatus::OK);
 }
 
-TEST_F(GyroscopeTests, gyroscope_Update_without_Init_first) {
+TEST_F(GyroscopeTests, Update_without_Init_first) {
   ConfigureUnitUnderTest();
 
-  auto gyroscope_update_return = unit_under_test_->Update();
-  EXPECT_EQ(gyroscope_update_return, types::DriverStatus::HAL_ERROR);
+  auto update_return = unit_under_test_->Update();
+  EXPECT_EQ(update_return, types::DriverStatus::HAL_ERROR);
 }
 
-TEST_F(GyroscopeTests, gyroscope_Get_without_Update_and_Init_first) {
+TEST_F(GyroscopeTests, Get_without_Update_and_Init_first) {
   ConfigureUnitUnderTest();
 
   types::EuclideanVector<std::int16_t> expected_value{-1, -1, -1};
-  auto gyroscope_get_return = unit_under_test_->Get();
+  auto get_return = unit_under_test_->Get();
 
-  EXPECT_EQ(gyroscope_get_return.x, expected_value.x);
-  EXPECT_EQ(gyroscope_get_return.y, expected_value.y);
-  EXPECT_EQ(gyroscope_get_return.z, expected_value.z);
+  EXPECT_EQ(get_return.x, expected_value.x);
+  EXPECT_EQ(get_return.y, expected_value.y);
+  EXPECT_EQ(get_return.z, expected_value.z);
 }
 
-TEST_F(GyroscopeTests, gyroscope_Get_without_Update_first) {
+TEST_F(GyroscopeTests, Get_without_Update_first) {
   ConfigureUnitUnderTest();
 
   types::EuclideanVector<std::int16_t> expected_value{0, 0, 0};
   unit_under_test_->Init(i2c_address_);
-  auto gyroscope_get_return = unit_under_test_->Get();
+  auto get_return = unit_under_test_->Get();
 
-  EXPECT_EQ(gyroscope_get_return.x, expected_value.x);
-  EXPECT_EQ(gyroscope_get_return.y, expected_value.y);
-  EXPECT_EQ(gyroscope_get_return.z, expected_value.z);
+  EXPECT_EQ(get_return.x, expected_value.x);
+  EXPECT_EQ(get_return.y, expected_value.y);
+  EXPECT_EQ(get_return.z, expected_value.z);
 }
 
-TEST_F(GyroscopeTests, gyroscope_Update_failes_in_ReadContentFromRegister) {
+TEST_F(GyroscopeTests, Update_failes_in_ReadContentFromRegister) {
   ON_CALL(*i2c_handler_, ReadContentFromRegister(_, imu::GYRO_XOUT_H, _, _))
       .WillByDefault(Return(answer_invalid));
 
@@ -119,20 +119,20 @@ TEST_F(GyroscopeTests, gyroscope_Update_failes_in_ReadContentFromRegister) {
   EXPECT_EQ(update_return, types::DriverStatus::HAL_ERROR);
 }
 
-TEST_F(GyroscopeTests, gyroscope_full) {
+TEST_F(GyroscopeTests, full) {
   ConfigureUnitUnderTest();
 
   types::EuclideanVector<std::int16_t> expected_value{2000, 0, -2000};
   unit_under_test_->Init(i2c_address_);
   unit_under_test_->Update();
-  auto gyroscope_get_return = unit_under_test_->Get();
+  auto get_return = unit_under_test_->Get();
 
-  EXPECT_EQ(gyroscope_get_return.x, expected_value.x);
-  EXPECT_EQ(gyroscope_get_return.y, expected_value.y);
-  EXPECT_EQ(gyroscope_get_return.z, expected_value.z);
+  EXPECT_EQ(get_return.x, expected_value.x);
+  EXPECT_EQ(get_return.y, expected_value.y);
+  EXPECT_EQ(get_return.z, expected_value.z);
 }
 
-TEST_F(GyroscopeTests, gyroscope_SetSensitivity_finest) {
+TEST_F(GyroscopeTests, SetSensitivity_finest) {
   const std::vector<std::uint8_t> expected_data_finest{imu::GYRO_CONFIG, 0b11100111};
   EXPECT_CALL(*i2c_handler_, Write(i2c_address_, expected_data_finest, _))
       .WillOnce(Return(types::DriverStatus::OK))
@@ -141,11 +141,11 @@ TEST_F(GyroscopeTests, gyroscope_SetSensitivity_finest) {
   ConfigureUnitUnderTest();
 
   unit_under_test_->Init(i2c_address_);
-  auto gyroscope_sensitivity_return = unit_under_test_->SetSensitivity(types::ImuSensitivity::FINEST);
-  EXPECT_EQ(gyroscope_sensitivity_return, types::DriverStatus::OK);
+  auto sensitivity_return = unit_under_test_->SetSensitivity(types::ImuSensitivity::FINEST);
+  EXPECT_EQ(sensitivity_return, types::DriverStatus::OK);
 }
 
-TEST_F(GyroscopeTests, gyroscope_SetSensitivity_finer) {
+TEST_F(GyroscopeTests, SetSensitivity_finer) {
   const std::vector<std::uint8_t> expected_data_finest{imu::GYRO_CONFIG, 0b11100111};
   const std::vector<std::uint8_t> expected_data_finer{imu::GYRO_CONFIG, 0b11101111};
 
@@ -157,11 +157,11 @@ TEST_F(GyroscopeTests, gyroscope_SetSensitivity_finer) {
   ConfigureUnitUnderTest();
 
   unit_under_test_->Init(i2c_address_);
-  auto gyroscope_sensitivity_return = unit_under_test_->SetSensitivity(types::ImuSensitivity::FINER);
-  EXPECT_EQ(gyroscope_sensitivity_return, types::DriverStatus::OK);
+  auto sensitivity_return = unit_under_test_->SetSensitivity(types::ImuSensitivity::FINER);
+  EXPECT_EQ(sensitivity_return, types::DriverStatus::OK);
 }
 
-TEST_F(GyroscopeTests, gyroscope_SetSensitivity_rougher) {
+TEST_F(GyroscopeTests, SetSensitivity_rougher) {
   const std::vector<std::uint8_t> expected_data_finest{imu::GYRO_CONFIG, 0b11100111};
   const std::vector<std::uint8_t> expected_data_rougher{imu::GYRO_CONFIG, 0b11110111};
 
@@ -173,11 +173,11 @@ TEST_F(GyroscopeTests, gyroscope_SetSensitivity_rougher) {
   ConfigureUnitUnderTest();
 
   unit_under_test_->Init(i2c_address_);
-  auto gyroscope_sensitivity_return = unit_under_test_->SetSensitivity(types::ImuSensitivity::ROUGHER);
-  EXPECT_EQ(gyroscope_sensitivity_return, types::DriverStatus::OK);
+  auto sensitivity_return = unit_under_test_->SetSensitivity(types::ImuSensitivity::ROUGHER);
+  EXPECT_EQ(sensitivity_return, types::DriverStatus::OK);
 }
 
-TEST_F(GyroscopeTests, gyroscope_SetSensitivity_roughest) {
+TEST_F(GyroscopeTests, SetSensitivity_roughest) {
   const std::vector<std::uint8_t> expected_data_finest{imu::GYRO_CONFIG, 0b11100111};
   const std::vector<std::uint8_t> expected_data_roughest{imu::GYRO_CONFIG, 0b11111111};
 
@@ -189,11 +189,11 @@ TEST_F(GyroscopeTests, gyroscope_SetSensitivity_roughest) {
   ConfigureUnitUnderTest();
 
   unit_under_test_->Init(i2c_address_);
-  auto gyroscope_sensitivity_return = unit_under_test_->SetSensitivity(types::ImuSensitivity::ROUGHEST);
-  EXPECT_EQ(gyroscope_sensitivity_return, types::DriverStatus::OK);
+  auto sensitivity_return = unit_under_test_->SetSensitivity(types::ImuSensitivity::ROUGHEST);
+  EXPECT_EQ(sensitivity_return, types::DriverStatus::OK);
 }
 
-TEST_F(GyroscopeTests, gyroscope_SetSensitivity_failes_stored_sensitivity_stays_same) {
+TEST_F(GyroscopeTests, SetSensitivity_failes_stored_sensitivity_stays_same) {
   ON_CALL(*i2c_handler_, ReadContentFromRegister(0, _, _, _))
       .WillByDefault(Return(answer_invalid));
 
@@ -201,41 +201,41 @@ TEST_F(GyroscopeTests, gyroscope_SetSensitivity_failes_stored_sensitivity_stays_
 
   unit_under_test_->Init(i2c_address_);
   auto set_sensitivity = types::ImuSensitivity::ROUGHEST;
-  auto gyroscope_sensitivity_return = unit_under_test_->SetSensitivity(set_sensitivity);
-  auto gyroscope_get = unit_under_test_->GetSensitivity();
-  EXPECT_EQ(gyroscope_sensitivity_return, types::DriverStatus::OK);
-  EXPECT_EQ(gyroscope_get, set_sensitivity);
+  auto sensitivity_return = unit_under_test_->SetSensitivity(set_sensitivity);
+  auto get = unit_under_test_->GetSensitivity();
+  EXPECT_EQ(sensitivity_return, types::DriverStatus::OK);
+  EXPECT_EQ(get, set_sensitivity);
 
   unit_under_test_->Init(0);
-  gyroscope_sensitivity_return = unit_under_test_->SetSensitivity(types::ImuSensitivity::FINEST);
-  EXPECT_EQ(gyroscope_sensitivity_return, types::DriverStatus::HAL_ERROR);
-  EXPECT_EQ(gyroscope_get, set_sensitivity);
+  sensitivity_return = unit_under_test_->SetSensitivity(types::ImuSensitivity::FINEST);
+  EXPECT_EQ(sensitivity_return, types::DriverStatus::HAL_ERROR);
+  EXPECT_EQ(get, set_sensitivity);
 }
 
-TEST_F(GyroscopeTests, gyroscope_SetSensitivity_without_Init_first) {
+TEST_F(GyroscopeTests, SetSensitivity_without_Init_first) {
   ConfigureUnitUnderTest();
 
-  auto gyroscope_sensitivity_return = unit_under_test_->SetSensitivity(types::ImuSensitivity::ROUGHEST);
-  EXPECT_EQ(gyroscope_sensitivity_return, types::DriverStatus::HAL_ERROR);
+  auto sensitivity_return = unit_under_test_->SetSensitivity(types::ImuSensitivity::ROUGHEST);
+  EXPECT_EQ(sensitivity_return, types::DriverStatus::HAL_ERROR);
 }
 
-TEST_F(GyroscopeTests, gyroscope_SetSensitivity_GetSensitivity) {
+TEST_F(GyroscopeTests, SetSensitivity_GetSensitivity) {
   ConfigureUnitUnderTest();
 
   unit_under_test_->Init(i2c_address_);
 
   auto set_sensitivity = types::ImuSensitivity::ROUGHEST;
   unit_under_test_->SetSensitivity(set_sensitivity);
-  auto gyroscope_get = unit_under_test_->GetSensitivity();
-  EXPECT_EQ(gyroscope_get, set_sensitivity);
+  auto get = unit_under_test_->GetSensitivity();
+  EXPECT_EQ(get, set_sensitivity);
 
   set_sensitivity = types::ImuSensitivity::FINEST;
   unit_under_test_->SetSensitivity(set_sensitivity);
-  gyroscope_get = unit_under_test_->GetSensitivity();
-  EXPECT_EQ(gyroscope_get, set_sensitivity);
+  get = unit_under_test_->GetSensitivity();
+  EXPECT_EQ(get, set_sensitivity);
 }
 
-TEST_F(GyroscopeTests, gyroscope_connection_failed_after_Mpu9255Detected) {
+TEST_F(GyroscopeTests, connection_failed_after_Mpu9255Detected) {
   EXPECT_CALL(*i2c_handler_, Write(_, _, _))
       .WillOnce(Return(types::DriverStatus::HAL_ERROR));
 
@@ -245,7 +245,7 @@ TEST_F(GyroscopeTests, gyroscope_connection_failed_after_Mpu9255Detected) {
   EXPECT_EQ(init_return, types::DriverStatus::HAL_ERROR);
 }
 
-TEST_F(GyroscopeTests, gyroscope_read_bytesize_mismatch) {
+TEST_F(GyroscopeTests, read_bytesize_mismatch) {
   ON_CALL(*i2c_handler_, ReadContentFromRegister(_, imu::WHO_AM_I_MPU9255_REGISTER, _, _))
       .WillByDefault(Return(answer_read_mismatch));
 
