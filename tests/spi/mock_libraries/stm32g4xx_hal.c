19a0bddd60f98b1c6651e3974c1d08745fa6b798
c0nwild <conradwild@hotmail.com>
2021-02-22 22:40:24 +0100

    Adapted spi interface unit test and harness

--- tests/spi/mock_libraries/stm32g4xx_hal.c
+++ tests/spi/mock_libraries/stm32g4xx_hal.c
@@ -5,4 +5,9 @@ HAL_StatusTypeDef HAL_SPI_TransmitReceive(SPI_HandleTypeDef *hspi, uint8_t *pTxD
   return hspi->mock_return_value;
 }
 
+HAL_StatusTypeDef HAL_SPI_Transmit(SPI_HandleTypeDef *hspi, uint8_t *pTxData, uint16_t Size,
+                                   uint32_t Timeout) {
+  return hspi->mock_return_value;
+}
+
 void HAL_GPIO_WritePin(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState) {}
