3f1b92adc0b86a1000d2b312c21d0895f5c1947f
c0nwild <conradwild@hotmail.com>
2021-02-26 19:55:30 +0100

    adapted spi unit tests

--- tests/spi/spi_tests.cpp
+++ tests/spi/spi_tests.cpp
@@ -16,27 +16,27 @@ class SPITests : public ::testing::Test {
   std::vector<uint8_t> mosi_buffer;
 };
 
-TEST_F(SPITests, exceeding_buffer_size_limit) {
+TEST_F(SPITests, transfer_exceeding_buffer_size_limit) {
   std::vector<uint8_t> too_large_buffer(65);
   types::DriverStatus rv = unit_under_test_->Transfer(mosi_buffer, too_large_buffer);
   EXPECT_EQ(rv, types::DriverStatus::INPUT_ERROR);
 }
 
-TEST_F(SPITests, buffer_size_limit_at_maximum) {
+TEST_F(SPITests, transfer_buffer_size_limit_at_maximum) {
   hspi1.mock_return_value = HAL_StatusTypeDef::HAL_OK;
   std::vector<uint8_t> miso_buffer(64);
   types::DriverStatus rv = unit_under_test_->Transfer(mosi_buffer, miso_buffer);
   EXPECT_EQ(rv, types::DriverStatus::OK);
 }
 
-TEST_F(SPITests, buffer_size_limit_zero) {
+TEST_F(SPITests, transfer_buffer_size_limit_zero) {
   hspi1.mock_return_value = HAL_StatusTypeDef::HAL_OK;
   std::vector<uint8_t> zero_size_buffer(0);
   types::DriverStatus rv = unit_under_test_->Transfer(zero_size_buffer, miso_buffer);
   EXPECT_EQ(rv, types::DriverStatus::OK);
 }
 
-TEST_F(SPITests, miso_buffer_too_small) {
+TEST_F(SPITests, transfer_miso_buffer_too_small) {
   hspi1.mock_return_value = HAL_StatusTypeDef::HAL_OK;
   std::vector<uint8_t> mosi_buffer(5);
   std::vector<uint8_t> small_miso_buffer(0);
@@ -44,27 +44,71 @@ TEST_F(SPITests, miso_buffer_too_small) {
   EXPECT_EQ(rv, types::DriverStatus::INPUT_ERROR);
 }
 
-TEST_F(SPITests, successful_spi_transfer) {
+TEST_F(SPITests, transfer_successful_spi_transfer) {
   hspi1.mock_return_value = HAL_StatusTypeDef::HAL_OK;
   types::DriverStatus rv = unit_under_test_->Transfer(miso_buffer, mosi_buffer);
   EXPECT_EQ(rv, types::DriverStatus::OK);
 }
 
-TEST_F(SPITests, timeout_during_spi_transfer) {
+TEST_F(SPITests, transfer_timeout_during_spi_transfer) {
   hspi1.mock_return_value = HAL_StatusTypeDef::HAL_TIMEOUT;
   types::DriverStatus rv = unit_under_test_->Transfer(miso_buffer, mosi_buffer);
   EXPECT_EQ(rv, types::DriverStatus::TIMEOUT);
 }
 
-TEST_F(SPITests, spi_hal_error) {
+TEST_F(SPITests, transfer_spi_hal_error) {
   hspi1.mock_return_value = HAL_StatusTypeDef::HAL_ERROR;
   types::DriverStatus rv = unit_under_test_->Transfer(miso_buffer, mosi_buffer);
   EXPECT_EQ(rv, types::DriverStatus::HAL_ERROR);
 }
 
-TEST_F(SPITests, spi_hal_busy) {
+TEST_F(SPITests, transfer_spi_hal_busy) {
   hspi1.mock_return_value = HAL_StatusTypeDef::HAL_BUSY;
   types::DriverStatus rv = unit_under_test_->Transfer(miso_buffer, mosi_buffer);
+  EXPECT_EQ(rv, types::DriverStatus::HAL_ERROR);
+}
+
+TEST_F(SPITests, write_exceeding_buffer_size_limit) {
+  std::vector<uint8_t> too_large_buffer(65);
+  types::DriverStatus rv = unit_under_test_->Write(too_large_buffer);
+  EXPECT_EQ(rv, types::DriverStatus::INPUT_ERROR);
+}
+
+TEST_F(SPITests, write_buffer_size_limit_at_maximum) {
+  hspi1.mock_return_value = HAL_StatusTypeDef::HAL_OK;
+  std::vector<uint8_t> mosi_buffer(64);
+  types::DriverStatus rv = unit_under_test_->Write(mosi_buffer);
+  EXPECT_EQ(rv, types::DriverStatus::OK);
+}
+
+TEST_F(SPITests, write_buffer_size_limit_zero) {
+  hspi1.mock_return_value = HAL_StatusTypeDef::HAL_OK;
+  std::vector<uint8_t> zero_size_buffer(0);
+  types::DriverStatus rv = unit_under_test_->Write(zero_size_buffer);
+  EXPECT_EQ(rv, types::DriverStatus::OK);
+}
+
+TEST_F(SPITests, write_successful_spi_transfer) {
+  hspi1.mock_return_value = HAL_StatusTypeDef::HAL_OK;
+  types::DriverStatus rv = unit_under_test_->Write(mosi_buffer);
+  EXPECT_EQ(rv, types::DriverStatus::OK);
+}
+
+TEST_F(SPITests, write_timeout_during_spi_transfer) {
+  hspi1.mock_return_value = HAL_StatusTypeDef::HAL_TIMEOUT;
+  types::DriverStatus rv = unit_under_test_->Write(mosi_buffer);
+  EXPECT_EQ(rv, types::DriverStatus::TIMEOUT);
+}
+
+TEST_F(SPITests, write_spi_hal_error) {
+  hspi1.mock_return_value = HAL_StatusTypeDef::HAL_ERROR;
+  types::DriverStatus rv = unit_under_test_->Write(mosi_buffer);
+  EXPECT_EQ(rv, types::DriverStatus::HAL_ERROR);
+}
+
+TEST_F(SPITests, write_spi_hal_busy) {
+  hspi1.mock_return_value = HAL_StatusTypeDef::HAL_BUSY;
+  types::DriverStatus rv = unit_under_test_->Write(mosi_buffer);
   EXPECT_EQ(rv, types::DriverStatus::HAL_ERROR);
 }
 
