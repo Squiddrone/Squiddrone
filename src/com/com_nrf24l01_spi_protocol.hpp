9a2cca18babb266e9e78cb74fac2512b4f48e8da
c0nwild <conradwild@hotmail.com>
2021-03-08 16:27:01 +0100

    solved conflicts

---
+++ src/com/com_nrf24l01_spi_protocol.hpp
@@ -0,0 +1,22 @@
+#ifndef SRC_COM_COM_NRF24L01_SPI_PROTOCOL_HPP_
+#define SRC_COM_COM_NRF24L01_SPI_PROTOCOL_HPP_
+
+#include "spi.hpp"
+
+namespace com {
+class NRF24L01SpiProtocol {
+ public:
+  NRF24L01SpiProtocol(spi::SPI &spi) : spi_(spi){};
+  // SPI protocol functions
+  auto ReadRegister(std::uint8_t register_address) noexcept -> std::uint8_t;
+  auto ReadRegister(std::uint8_t register_address, std::uint8_t length) noexcept -> std::vector<uint8_t>;
+  auto WriteRegister(std::uint8_t register_address, std::uint8_t register_content) -> types::DriverStatus;
+  auto WriteRegister(std::uint8_t register_address, std::vector<uint8_t> register_content) -> types::DriverStatus;
+  auto WritePayloadData(std::vector<uint8_t> &payload) noexcept -> types::DriverStatus;
+
+ private:
+  spi::SPI &spi_;
+};
+}  // namespace com
+
+#endif
