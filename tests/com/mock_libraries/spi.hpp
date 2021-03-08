eb3c9cf7fecabd3a872f7fe6a169c75fc58ce514
c0nwild <conradwild@hotmail.com>
2021-03-08 17:14:15 +0100

    added mock definitions for the test harness

---
+++ tests/com/mock_libraries/spi.hpp
@@ -0,0 +1,28 @@
+#ifndef TESTS_MOCK_LIBRARIES_COM_MOCK_SPI_HPP_
+#define TESTS_MOCK_LIBRARIES_COM_MOCK_SPI_HPP_
+
+#include "error_types.hpp"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+typedef struct
+{
+} GPIO_TypeDef;
+
+namespace spi {
+
+typedef struct CSPinDefinition {
+  GPIO_TypeDef *peripheral;
+  uint16_t gpio_pin;
+} CSPin;
+
+class MockSPI {
+ public:
+  MockSPI(const CSPin chip_select){};
+  MOCK_METHOD(types::DriverStatus, Transfer, (std::vector<uint8_t> & mosi_data_buffer, std::vector<uint8_t> &miso_data_buffer), (noexcept));
+  MOCK_METHOD(types::DriverStatus, Write, (std::vector<uint8_t> & mosi_data_buffer), (noexcept));
+};
+
+using SPI = MockSPI;
+}  // namespace spi
+#endif
