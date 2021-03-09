3b9405111a8e8f1015a7fb0d360564ed49d08b1b
c0nwild <conradwild@hotmail.com>
2021-03-09 21:43:11 +0100

    added some doxygen documentation

--- src/spi/cspin.hpp
+++ src/spi/cspin.hpp
@@ -35,12 +35,30 @@ class CSPin {
   auto MapActiveStateToPinSetting() -> void;
 
  public:
+  /**
+  * @brief Construct a new CSPin object. 
+  * 
+  * @param peripheral Pointer to the GPIO Peripheral that acts as chip select pin.
+  * @param gpio_pin Pin number of the GPIO Peripheral that acts as chip select pin.
+  * @param active_state Can be active high or active low, depending on the necessary SPI configuration.
+  */
   explicit CSPin(GPIO_TypeDef *peripheral,
                  uint16_t gpio_pin,
                  CSActiveState active_state) : peripheral_(peripheral),
                                                gpio_pin_(gpio_pin),
                                                active_state_(active_state) { MapActiveStateToPinSetting(); };
+  /**
+   * @brief Activate the chip select pin. Actual pin value (high or low) depends on the 
+   * selection of active_state.
+   * 
+   */
   auto SetCSActive() noexcept -> void;
+
+  /**
+   * @brief Deactivate the chip select pin. Actual pin value (high or low) depends on the 
+   * selection of active_state.
+   * 
+   */
   auto SetCSInactive() noexcept -> void;
 };
 
