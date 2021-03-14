74a9b3b8457c69cc7c36fbe1116a43c955b11722
c0nwild <conradwild@hotmail.com>
2021-03-14 22:16:40 +0100

    added spi protocol testcases

--- tests/com/com_nrf24l01_spi_protocol_tests.cpp
+++ tests/com/com_nrf24l01_spi_protocol_tests.cpp
@@ -60,6 +60,24 @@ TEST_F(ComNRF24L01SpiProtocolTests, write_register_multibyte_spi_tx_ok) {
   EXPECT_EQ(retval, types::DriverStatus::OK);
 }
 
+TEST_F(ComNRF24L01SpiProtocolTests, write_payload_spi_tx_ok) {
+  std::vector<std::uint8_t> test_reg_content{0xaf, 0xaf, 0xaf};
+  unit_under_test_ = std::make_unique<com::NRF24L01SpiProtocol>(spi_);
+  EXPECT_CALL(spi_, Write(ElementsAre(com::instruction_word::W_TX_PAYLOAD, 0xaf, 0xaf, 0xaf)));
+  auto retval = unit_under_test_->WritePayloadData(test_reg_content);
+  EXPECT_EQ(retval, types::DriverStatus::OK);
+}
+
+TEST_F(ComNRF24L01SpiProtocolTests, read_payload_spi_tx_ok) {
+  std::vector<std::uint8_t> test_payload;
+  test_payload.resize(4);
+  unit_under_test_ = std::make_unique<com::NRF24L01SpiProtocol>(spi_);
+  EXPECT_CALL(spi_, Transfer(ElementsAre(com::instruction_word::R_RX_PAYLOAD), _));
+  auto retval = unit_under_test_->ReadPayloadData(test_payload);
+  EXPECT_THAT(test_payload, ElementsAre(_, _, _));
+  EXPECT_EQ(retval, types::DriverStatus::OK);
+}
+
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
