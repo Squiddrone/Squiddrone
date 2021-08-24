9daa21f6cf850bb35a1b537e90b298f71f470d7e
c0nwild <conradwild@hotmail.com>
2021-08-24 21:53:32 +0200

    updated com interface definition

--- src/com/com_interface.hpp
+++ src/com/com_interface.hpp
@@ -49,13 +49,14 @@ class ComInterface {
   /** 
    * @brief Transmit dataframe of max length 32 byte via the drones' wifi interface.
    * 
-   * @param target_id target_id The wifi id of the receiver.
+   * @param target_id target_id The id of the receiver. 0 - front, 1 - back, 2 - left, 3 - right,
+   * 4 - above, 5 - below and 255 - ground control.
    * @param payload payload An array of bytes. Max length 32 bytes. Must conform to SquidDrone 
    * com protocol specification.
    * @return types::ComError COM_OK in case of successful transmission, error code in case something 
    * went wrong.
    */
-  virtual auto PutDataPacket(std::uint8_t target_id, types::com_msg_frame &payload) noexcept
+  virtual auto PutDataPacket(types::PutDataTarget target_id, types::com_msg_frame &payload) noexcept
       -> types::DriverStatus = 0;
 
  protected:
