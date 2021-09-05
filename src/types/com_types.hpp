c535c2d29434212d164145c5ec4f6c3d01239c30
c0nwild <conradwild@hotmail.com>
2021-09-05 10:22:14 +0200

    added com target id definition

--- src/types/com_types.hpp
+++ src/types/com_types.hpp
@@ -11,6 +11,16 @@ static constexpr std::uint8_t COM_MAX_FRAME_LENGTH = 32;
 
 /// Type alias for com message frame datatype
 using com_msg_frame = std::vector<std::uint8_t>;
+
+enum class PutDataTarget : std::uint8_t {
+  TARGET_FRONT = 0x0,
+  TARGET_BACK,
+  TARGET_LEFT,
+  TARGET_RIGHT,
+  TARGET_ABOVE,
+  TARGET_BELOW,
+  TARGET_GROUND_CONTROL = 0xff
+};
 }  // namespace types
 
 #endif
