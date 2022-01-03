a35946157dbd182fcbf5cf9adae3687d6a57d1e5
c0nwild <conradwild@hotmail.com>
2022-01-03 07:26:46 +0100

    Data model change.
    Interface now uses data packets of type ComDataPacket

--- src/com/com_interface.hpp
+++ src/com/com_interface.hpp
@@ -42,21 +42,20 @@ class ComInterface {
   /** 
    * @brief Get the data packet object
    * 
-   * @return types::com_msg_frame Returns a dataframe array.
+   * @return types::ComDataPacket Returns a data packet of type types::ComDataPacket.
    */
-  virtual auto GetDataPacket() const noexcept -> types::com_msg_frame = 0;
+  virtual auto GetDataPacket() const noexcept -> types::ComDataPacket = 0;
 
   /** 
    * @brief Transmit dataframe of max length 32 byte via the drones' wifi interface.
    * 
    * @param target_id target_id The id of the receiver. 0 - front, 1 - back, 2 - left, 3 - right,
    * 4 - above, 5 - below and 255 - ground control.
-   * @param payload payload An array of bytes. Max length 32 bytes. Must conform to SquidDrone 
-   * com protocol specification.
-   * @return types::ComError COM_OK in case of successful transmission, error code in case something 
+   * @param packet A data packet of type types::ComDataPacket
+   * @return types::DriverStatus COM_OK in case of successful transmission, error code in case something 
    * went wrong.
    */
-  virtual auto PutDataPacket(types::PutDataTarget target_id, types::com_msg_frame &payload) noexcept
+  virtual auto PutDataPacket(types::PutDataTarget target_id, types::ComDataPacket &packet) noexcept
       -> types::DriverStatus = 0;
 
  protected:
